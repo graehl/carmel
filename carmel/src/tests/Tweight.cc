a1d0a4ba35227a1416569d55ab7b81b682614a21
Jonathan Graehl <graehl@gmail.com>
2002-08-26 19:38:14 +0000

    Interactive test of weight logarithmic arithmetic routines.
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@153 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ carmel/src/tests/Tweight.cc
@@ -0,0 +1,10 @@
+#include "../weight.cc"
+main()
+{
+  Weight a,b;
+  while(cin) {
+    cin >> a >> b;
+    cout << "a=" << a << " b=" << b << " a*b=" << a*b << " a/b=" << a/b << " a+b=" << a+b << " a-b=" << a-b << endl;
+    
+  }
+}
