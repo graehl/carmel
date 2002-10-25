355c10b21f296630c8fbb53ce061b93bee58c72a
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2002-10-25 01:28:05 +0000

    template for proper modern << and >> operators for user types
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@158 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/genio.h
@@ -0,0 +1,45 @@
+// your class Arg must will provide Arg::get_from(is) ( is >> arg ) and Arg::print_to(os) (os << arg), 
+// returning std::ios_base::iostate (0,badbit,failbit ...)
+// usage:
+/*
+template <class charT, class Traits>
+std::basic_istream<class charT, class Traits>&
+operator >>
+ (std::basic_istream<class charT, class Traits>& is, Arg &arg)
+{
+	return gen_extractor(is,arg);
+}
+*/
+// this is necessary because of incomplete support for partial explicit template instantiation
+// a more complete version could catch exceptions and set ios error
+
+template <class charT, class Traits, class Arg>
+std::basic_istream<charT, Traits>& 
+gen_extractor
+(std::basic_istream<charT, Traits>& s, Arg &arg)
+{
+	if (!s.good()) return s;
+	std::ios_base::iostate err = 0;
+	typename std::basic_istream<charT, Traits>::sentry sentry(s);
+	if (sentry)
+		err = arg.get_from(s);
+	if (err)
+		s.setstate(err);
+	return s;
+}
+
+// exact same as above but with o instead of i
+template <class charT, class Traits, class Arg>
+std::basic_ostream<charT, Traits>& 
+gen_inserter
+	(std::basic_ostream<charT, Traits>& s, const Arg &arg)
+{
+	if (!s.good()) return s;
+	std::ios_base::iostate err = 0;
+	typename std::basic_ostream<charT, Traits>::sentry sentry(s);
+	if (sentry)
+		err = arg.print_on(s);
+	if (err)
+		s.setstate(err);
+	return s;
+}
