1d191aa1d0490a57e0ee5e87e6490eeb2052b448
Jonathan Graehl <graehl@gmail.com>
2002-10-29 20:41:13 +0000

    exercised Weight::out_* manipulators
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@171 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- carmel/src/tests/Tweight.cc
+++ carmel/src/tests/Tweight.cc
@@ -1,10 +1,15 @@
-#include "../weight.cc"
+#include "../weight.h"
+using namespace std;
 main()
 {
   Weight a,b;
-  while(cin) {
+  for (;;) {
     cin >> a >> b;
-    cout << "a=" << a << " b=" << b << " a*b=" << a*b << " a/b=" << a/b << " a+b=" << a+b << " a-b=" << a-b << endl;
-    
+	if (cin) {
+	cout << Weight::out_ln << Weight::out_always_real << "a=" << a << " b=" << b << " a*b=" << a*b << " a/b=" << a/b << " a+b=" << a+b << " a-b=" << a-b << endl;
+	cout << Weight::out_always_log << "a=" << a << " b=" << b << " a*b=" << a*b << " a/b=" << a/b << " a+b=" << a+b << " a-b=" << a-b << endl;
+	cout << Weight::out_variable << "a=" << a << " b=" << b << " a*b=" << a*b << " a/b=" << a/b << " a+b=" << a+b << " a-b=" << a-b << endl;
+	} else
+		break;
   }
 }
