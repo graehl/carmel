bc9cdb88acca8517506ac84cad28746147e2c423
Jonathan Graehl <graehl@gmail.com>
2003-05-21 00:14:51 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@239 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- carmel/src/models.h
+++ carmel/src/models.h
@@ -7,21 +7,12 @@ char *ModelsDef[] = {
   "0 (0 (0 \"A\" \"A\" 0.75)  (0 \"AA\" \"A\" 0.25) (0 \"B\" \"B\" 0.67) (0 \"BB\" \"B\" 0.33))",
   "0 (0 (0 \"A\" \"a\") (0 \"B\" \"b\"))"
 };
-// Define your first model
-#define M1 "0 (0 (0 \"A\" \"A\" 0.75)  (0 \"AA\" \"A\" 0.25) (0 \"B\" \"B\" 0.67) (0 \"BB\" \"B\" 0.33))\0"
-
-// Define your second model
-#define M2 "0 (0 (0 \"A\" \"a\") (0 \"B\" \"b\")) \0"
-
-// define additional models if neede and name them M3, M4, ...etc.
+// define additional models if needed
 
 
 vector<string> Models ;
 void initModels()
 {
-  // Note: order here is important 
-//  Models.push_back(M1); // add your first model to the list
-//  Models.push_back(M2); // add your second model to the list
   // Similarly, add additional models if necessary
   int n_models = sizeof(ModelsDef)/sizeof(char *);
   for (int i=0;i<n_models;++i)
