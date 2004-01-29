7d0793ab2b3e076d549393c6653372fe402bcd59
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2004-01-29 01:49:10 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@268 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/test.hpp
@@ -0,0 +1,9 @@
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4267 4535 )
+#endif
+#include <boost/test/included/unit_test_framework.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
