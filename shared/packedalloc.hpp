91e97573c7f10620d6d7fcfbbda20408075d96df
Jonathan Graehl <graehl@gmail.com>
2004-04-09 00:27:01 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@280 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/packedalloc.hpp
@@ -0,0 +1,68 @@
+#ifndef _PACKEDALLOC_HPP
+#define _PACKEDALLOC_HPP
+// no separate implementation for now, just #define MAIN in one source file that includes this
+
+#include <memory>
+
+#ifndef PACKED_ALLOC_BLOCKSIZE
+#ifdef TEST
+#define PACKED_ALLOC_BLOCKSIZE 8
+#else
+#define PACKED_ALLOC_BLOCKSIZE 4096
+#endif
+#endif
+
+template <class C,class Alloc=std::allocator<C>,size_t block_size=PACKED_ALLOC_BLOCKSIZE > class PackedAlloc {
+  Alloc alloc;
+  typedef std::pair<C *,size_t> Block;
+  typedef List<Block> ListB;
+  ListB blocks;
+  C *free_start;
+  C *free_end;
+public:
+  C * allocate(size_t n) {
+	if ( free_end >= free_start+n) {
+	  C *ret=free_start;
+	  free_start+=n;
+	  return ret;
+	} else {
+	  C *ret;
+	  if (n > block_size)  // one-off unique block just big enough
+		ret=new_block(n);
+	  else { // standard sized block with room to spare
+	    ret=new_block();
+		free_start = ret + n;
+		free_end = ret + block_size;		
+	  }
+	  
+  	  return ret;
+	}
+  }
+  C *new_block(size_t n=block_size) {
+	C *ret=alloc.allocate(n);
+	blocks.push_front(Block(ret,n));
+	return ret;
+  }
+  void deallocate(C *p, size_t n) {}
+  PackedAlloc() {
+	free_start=free_end=0;
+  }
+  ~PackedAlloc() {
+	for (ListB::const_iterator i=blocks.const_begin(),end=blocks.const_end();i!=end;++i)
+	  alloc.deallocate(i->first,i->second);
+  }
+};
+
+template <class C,class Alloc=std::allocator<C>,size_t block_size=PACKED_ALLOC_BLOCKSIZE > struct StaticPackedAlloc {
+  static PackedAlloc<C,Alloc,block_size> alloc;
+  C * allocate(size_t n) {
+	return alloc.allocate(n);
+  }
+  void deallocate(C *p, size_t n) {alloc.deallocate(p,n)}
+};
+
+#ifdef MAIN
+template <class C,class Alloc,size_t block_size> PackedAlloc<C,Alloc,block_size> StaticPackedAlloc<C,Alloc,block_size>::alloc;
+#endif
+
+#endif
