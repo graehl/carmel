c973c2c552fb4d7e71e8e190e18ce27b8eb5d730
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2004-06-10 03:26:31 +0000

    for tt
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@313 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/myassert.h
+++ shared/myassert.h
@@ -2,11 +2,33 @@
 #define ASSERT_H 1
 #include "config.h"
 #include <cassert>
+
 #ifdef DEBUG
-#define Assert(a) assert(a)
-#define Paranoid(a) do { a; } while (0)
+//#define Assert(a) assert(a)
+
+#ifdef _MSVC_VER
+#define BREAKPOINT assert(false)
 #else
+
+#define BREAKPOINT asm("   int $3")
+//#define BREAKPOINT   *(int *)0 = 0
+
+#endif
+
+#define Assert(expr) (expr) ? (void)0 :   \
+                 _my_assert(__FILE__,__LINE__,#expr)
+                 
+inline static void _my_assert(const char *file,unsigned line,const char *expr) 
+{
+  Config::err() << file << "(" << line << ") Assertion failed: " << expr << std::endl;
+  BREAKPOINT;
+}
+#define Paranoid(a) do { a; } while (0)
+
+#else
+#define BREAKPOINT 
 #define Assert(a)
 #define Paranoid(a)
+
 #endif
 #endif
