5e3dd33136b24e5d1a8529e2619531da7d54cae6
ithayer <ithayer@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2004-07-07 22:20:31 +0000

    Added some comments.
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@325 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/Debug.h
+++ shared/Debug.h
@@ -11,36 +11,40 @@ using namespace __gnu_cxx;
 #endif
 using namespace std;
 
-//! Debug: A short description of Debug
-/*! A longer description of Debug
+//! Debug: This is a class to print out debugging information
+/*! This should be used for most communication to the user. The reason for this
+  is to make sure that all of our debugging messages are consistent (esp
+  important for perl readability), and that if for some reason we want to
+  redirect error/warning messages to different files, this can be done easily.
 */
 
 class Debug {
 public:
   Debug() : debugOS(cerr), infoOS(cout) {};
   
-  inline ostream &getDebugOutput() {
+  inline ostream &getDebugOutput() {			   //!< Get the strream to which debugging output is written
     return debugOS;
   }
 
-  inline ostream &getInfoOutput() { 
+  inline ostream &getInfoOutput() { 			   //!< Get the stream to which informational output is written
     return infoOS;
   }
   
-  void error(const string &module, const string &info, const string &file="", const int line=0) { // prints an error
+  void error(const string &module, const string &info, const string &file="", const int line=0) { //!< prints an error
     getDebugOutput() << "::" << module << "(" << file << ":" << line << "): ERROR: " << info << endl;    
   }
   
-  void fatalError(const string &module, const string &info, const string &file="", const int line=0) { // prints an error and dies
+  void fatalError(const string &module, const string &info, const string &file="", const int line=0) { //!< prints an error and dies
     error(module, info, file, line);
+    assert(0);
     exit(-1);
   }
 
-  void warning(const string &module, const string &info, const string &file="", const int line=0) { 
+  void warning(const string &module, const string &info, const string &file="", const int line=0) { //!< prints a warning message
     getDebugOutput() << "::" << module << "(" << file << ":" << line << "): WARNING: " << info << endl;    
   }		
 
-  void info(const string &module, const string &info, const string &file="", const int line=0) { 
+  void info(const string &module, const string &info, const string &file="", const int line=0) { //!< prints an informational message
     if (file=="") { 
       getInfoOutput() << module << ": " << info << endl;
     } else { 
