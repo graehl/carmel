b35966009567f0df591ee4c672db9a42cdf1a822
Jonathan Graehl <graehl@gmail.com>
2004-08-28 00:19:51 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@380 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/myassert.h
+++ shared/myassert.h
@@ -10,15 +10,18 @@
 #define BREAKPOINT __asm int 3
 #else
 
+#ifdef __i386__
 #define BREAKPOINT asm("   int $3")
-//#define BREAKPOINT   *(int *)0 = 0
+#else
+#define BREAKPOINT   *(int *)0 = 0
+#endif
 
 #endif
 
 #define Assert(expr) (expr) ? (void)0 :   \
                  _my_assert(__FILE__,__LINE__,#expr)
-                 
-inline static void _my_assert(const char *file,unsigned line,const char *expr) 
+
+inline static void _my_assert(const char *file,unsigned line,const char *expr)
 {
   Config::err() << file << "(" << line << ") Assertion failed: " << expr << std::endl;
   BREAKPOINT;
@@ -26,7 +29,7 @@ inline static void _my_assert(const char *file,unsigned line,const char *expr)
 #define Paranoid(a) do { a; } while (0)
 
 #else
-#define BREAKPOINT 
+#define BREAKPOINT
 #define Assert(a)
 #define Paranoid(a)
 
