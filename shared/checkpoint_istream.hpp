cba8c0757061e19c0c40fe0950fedc76b262f3f3
Jonathan Graehl <graehl@gmail.com>
2004-09-22 02:07:59 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@400 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/checkpoint_istream.hpp
+++ shared/checkpoint_istream.hpp
@@ -1,7 +1,28 @@
 #ifndef CHECKPOINT_ISTREAM_HPP
 #define CHECKPOINT_ISTREAM_HPP
 
+#include <fstream>
 
-
+struct checkpoint_istream {
+    std::fstream *f;
+    typedef std::streampos pos;
+    pos saved_pos;
+    init(std::fstream *f_) {
+        f=f_;
+        commit();
+    }
+    checkpoint_istream() : f(NULL) {}
+    checkpoint_istream(explicit std::istream &i) : {
+        init(&dynamic_cast<std::fstream &>(i));
+    }
+    void revert() {
+        f.seekg(saved_pos);
+    }
+    void commit() {
+        saved_pos=f.tellg();
+    }
+    operator fstream & () { return *f; }
+    operator bool () { return *f; } // can't double implicit convert
+};
 
 #endif
