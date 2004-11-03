117d71ea30b6e2093aff87ee50cc296ed581d6d2
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2004-11-03 00:22:55 +0000

    sortpercent minmax
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@490 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/io.hpp
+++ shared/io.hpp
@@ -18,9 +18,9 @@ struct DefaultReader
 
 struct DefaultWriter
 {
-  template <class charT, class Traits,class Label>
+  template <class charT, class Traits,class value_type>
         std::basic_ostream<charT,Traits>&
-         operator()(std::basic_ostream<charT,Traits>& o,const Label &l) const {
+         operator()(std::basic_ostream<charT,Traits>& o,const value_type &l) const {
           return o << l;
          }
 };
@@ -31,7 +31,7 @@ struct RandomReader
     typedef Label value_type;
     double prob_keep;
     RandomReader(double prob_keep_) : prob_keep(prob_keep_) {}
-    RandomReader(const RandomReader<Label> &r) : prob_keep(r.prob_keep) {}
+    RandomReader(const RandomReader<value_type> &r) : prob_keep(r.prob_keep) {}
     template <class charT, class Traits>
     std::basic_istream<charT,Traits>&
     operator()(std::basic_istream<charT,Traits>& in,value_type &l) const {
@@ -43,6 +43,7 @@ struct RandomReader
     }
 };
 
+//FIXME: serious bug: last item always oversampled on delimeter (because we don't set eof/fail)
 //FIXME: need for this (really parsing outside context) indicates that readers
 //should return bool if they want a potential retry instead
 template <class Label
@@ -53,19 +54,72 @@ struct RandomReaderTerm
     typedef Label value_type;
     double prob_keep;
     RandomReaderTerm(double prob_keep_) : prob_keep(prob_keep_) {}
-    RandomReaderTerm(const RandomReaderTerm<Label,terminator> &r) : prob_keep(r.prob_keep) {}
+    RandomReaderTerm(const RandomReaderTerm<value_type,terminator> &r) : prob_keep(r.prob_keep) {}
+    template <class charT, class Traits>
+    std::basic_istream<charT,Traits>&
+    operator()(std::basic_istream<charT,Traits>& in,value_type &l) const {
+        in >> l;
+        while (in && random01() >= prob_keep) {
+#define END_IF_TERMINATOR(terminator)  \
+                char c; \
+                if (!(in >> c)) \
+                    break; \
+                in.unget(); \
+                if (c==terminator) { \
+                    break; \
+                }
+            END_IF_TERMINATOR(terminator);
+//                    in.setstate(std::ios::failbit );   \
+
+            in >> l;
+        }
+        return in;
+    }
+};
+
+
+template <class Reader
+        ,char terminator=')'
+          >
+struct RandomReadWrapperTerm
+{
+    typedef typename Reader::value_type value_type;
+    Reader reader;
+    double prob_keep;
+    RandomReadWrapperTerm(const Reader &reader_,double prob_keep_) : prob_keep(prob_keep_),reader(reader_) {}
+    RandomReadWrapperTerm(const RandomReadWrapperTerm<value_type,terminator> &r) : prob_keep(r.prob_keep),reader(r.reader) {}
     template <class charT, class Traits>
     std::basic_istream<charT,Traits>&
     operator()(std::basic_istream<charT,Traits>& in,value_type &l) const {
             in >> l;
             while (in && random01() >= prob_keep) {
-                char c;
-                if (!(in >> c))
-                    break;
-                in.unget();
-                if (c==terminator)
-                    break;
+                END_IF_TERMINATOR(terminator);
+                deref(reader)(in,l);                // only difference
+//                in >> l;
+            }
+            return in;
+    }
+};
+
+template <class Label, class F
+        ,char terminator=')'
+          >
+struct RandomReaderTermCallback
+{
+    typedef Label value_type;
+    F f;
+    double prob_keep;
+    RandomReaderTermCallback(const F &f_,double prob_keep_) : prob_keep(prob_keep_),f(f_) {}
+    RandomReaderTermCallback(const RandomReaderTermCallback<value_type,F,terminator> &r) : prob_keep(r.prob_keep),f(r.f) {}
+    template <class charT, class Traits>
+    std::basic_istream<charT,Traits>&
+    operator()(std::basic_istream<charT,Traits>& in,value_type &l)  {
+            in >> l;
+            while (in && random01() >= prob_keep) {
+                END_IF_TERMINATOR(terminator);
+#undef END_IF_TERMINATOR
                 in >> l;
+                deref(f)(l);                // only difference
             }
             return in;
     }
@@ -103,7 +157,7 @@ struct ProgressReader
     typedef Label value_type;
     template <class charT, class Traits>
     std::basic_istream<charT,Traits>&
-    operator()(std::basic_istream<charT,Traits>& in,Label &l) const {
+    operator()(std::basic_istream<charT,Traits>& in,value_type &l) const {
         deref(tick)();
         return in >> l;
     }
