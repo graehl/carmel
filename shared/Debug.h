584b647a5cd3867d4e229273bbd8c9e36f903828
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2005-01-14 01:33:28 +0000

    debug macros like INFO allow << operator, and NO_INFO and INFO_LEVEL result in not building ostringstreams for unused messages
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@563 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/Debug.h
+++ shared/Debug.h
@@ -1,5 +1,5 @@
 // ;; -*- mode: C++; fill-column: 80; comment-column: 59; -*-
-// ;; 
+// ;;
 // ignacio
 
 #ifndef __DEBUG_H__
@@ -7,62 +7,78 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
 #ifndef __NO_GNU_NAMESPACE__
 using namespace __gnu_cxx;
 #endif
 using namespace std;
 
-#define DBG_OP(pDbg,op,module,msg) pDbg->op(module,msg,__FILE__,__LINE__)
-#define DBG_OP_Q(pDbg,op,module,msg) pDbg->op(module,msg)
+#ifndef INFO_LEVEL
+# define INFO_LEVEL 9999
+#endif
 
-namespace ns_decoder_global { 
-  
+#ifdef NO_INFO
+# define DBG_OP_F(pDbg,op,module,msg,file,line)
+#else
+# define DBG_OP_F(pDbg,op,module,msg,file,line) do { \
+  if (INFO_LEVEL > 0) { \
+   ostringstream os; \
+   os << msg; \
+   pDbg->op(module,os.str(),file,line);          \
+} } while(0)
+#endif
+
+#define DBG_OP(pDbg,op,module,msg) DBG_OP_F(pDbg,op,module,msg,__FILE__,__LINE__)
+#define DBG_OP_Q(pDbg,op,module,msg) DBG_OP_F(pDbg,op,module,msg,"",0)
+
+namespace ns_decoder_global {
+
   //! Debug: This is a class to print out debugging information
   /*! This should be used for most communication to the user. The reason for this
     is to make sure that all of our debugging messages are consistent (esp
     important for perl readability), and that if for some reason we want to
     redirect error/warning messages to different files, this can be done easily.
   */
-  
+
   class Debug {
   public:
     Debug() : debugOS(cerr), infoOS(cout) {};
-    
-    inline ostream &getDebugOutput() {			   //!< Get the strream to which debugging output is written
+
+    inline ostream &getDebugOutput() {                     //!< Get the strream to which debugging output is written
       return debugOS;
     }
-    
-    inline ostream &getInfoOutput() { 			   //!< Get the stream to which informational output is written
+
+    inline ostream &getInfoOutput() {                      //!< Get the stream to which informational output is written
       return infoOS;
     }
-    
+
     void error(const string &module, const string &info, const string &file="", const int line=0) { //!< prints an error
-      getDebugOutput() << "::" << module << "(" << file << ":" << line << "): ERROR: " << info << endl;    
+      getDebugOutput() << "::" << module << "(" << file << ":" << line << "): ERROR: " << info << endl;
     }
-    
+
     void fatalError(const string &module, const string &info, const string &file="", const int line=0) { //!< prints an error and dies
       error(module, info, file, line);
       assert(0);
       exit(-1);
     }
-    
+
     void warning(const string &module, const string &info, const string &file="", const int line=0) { //!< prints a warning message
-      getDebugOutput() << "::" << module << "(" << file << ":" << line << "): WARNING: " << info << endl;    
-    }		
-    
+      getDebugOutput() << "::" << module << "(" << file << ":" << line << "): WARNING: " << info << endl;
+    }
+
     void info(const string &module, const string &info, const string &file="", const int line=0) { //!< prints an informational message
-      if (file=="") { 
-	getInfoOutput() << module << ": " << info << endl;
-      } else { 
-	getInfoOutput() << module << "(" << file << ":" << line << "): " << info << endl;
+      if (file=="") {
+        getInfoOutput() << module << ": " << info << endl;
+      } else {
+        getInfoOutput() << module << "(" << file << ":" << line << "): " << info << endl;
       }
     }
-    
+
   private:
-    
-    ostream &debugOS;					   //!< output stream where error/WARNING messages are sent
-    ostream &infoOS;					   //!< output stream where debugging information is sent
+
+    ostream &debugOS;                                      //!< output stream where error/WARNING messages are sent
+    ostream &infoOS;                                       //!< output stream where debugging information is sent
   };
 }
 
