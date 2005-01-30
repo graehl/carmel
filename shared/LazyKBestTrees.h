b96e72d0fa4268daf85cc7632e812b07463ffbce
Jonathan Graehl <graehl@gmail.com>
2005-01-30 05:12:56 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@585 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/LazyKBestTrees.h
+++ shared/LazyKBestTrees.h
@@ -16,10 +16,16 @@ using namespace std;
   struct result_F {
      typedef int result_T;
      typedef int label_T;
-     static void set_null(result_T &result) { result=-1; }
-     static bool is_null(result_T result) { return result==-1; }
-     result_T operator ()(label_T label,result_T unary) const { return unary+label;}
-     result_T operator ()(label_T label,result_T left,result_T right) const { return left+right+label;}
+     static void set_end(result_T &result) { result=-1; }
+     static bool is_end(result_T result) { return result==-1; }
+     static void set_pending(result_T &result) { result=-2; }
+     static bool is_pending(result_T result) { return result==-2; }
+     result_T operator ()(label_T label) const { return label;} // leaf
+     result_T operator ()(label_T label,result_T unary) const { return unary+label;} // unary
+     result_T operator ()(label_T label,result_T left,result_T right) const { return left+right+label;} // binary
+     bool operator <(result_T a, result_T b) const { // < means cheaper/better
+        return a < b;
+     }
   };
 */
 
@@ -33,19 +39,28 @@ struct LazyKBest {
     struct OR;
     struct AND;
 
+    struct BestMemo {
+        vector<Result> kbest;        
+    };
+    
+    
     // both OR/AND: the kbest vector always contains cached 1...n best, or
     // result.is_null(kbest[kbest.size()-1]) if no more can be generated
     struct OR {
         typedef pair<cost_T,unsigned> Instance; // index of OR-child to take next-best from
         vector<Instance> queue; // cheapest first
         vector<AND *> children;
-        vector<Result> kbest;
+        vector<Result> kbest;        
     };
-    struct AND {
+    struct AND { // unary doesn't use queue.  leaf doesn't use anything except label
+        Label label;
         typedef pair<cost_T,pair<unsigned,unsigned> > Instance; // index of (left,right)-child to take next-best from
-        vector<Instance> queue; // cheapest first        
+        vector<Instance> queue; // cheapest first
         pair<OR *,OR *> children;
         vector<Result> kbest;
+        bool isUnary() const {
+            return children.second==NULL;
+        }
     };    
     
 };
