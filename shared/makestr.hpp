0d99c52187a82cb3b2de7770fdcbd8eca8e2af3f
Jonathan Graehl <graehl@gmail.com>
2005-04-12 06:37:27 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@728 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/makestr.hpp
+++ shared/makestr.hpp
@@ -11,11 +11,15 @@
 #define MAKESTR_DATE __DATE__ " " __TIME__
 
 // __FILE__  ":"  __LINE__  "): failed to write " #expr
-// usage: MAKESTRS(str,os << 1;os << 2 => str="12"
-#define MAKESTRS(string,expr) do {std::ostringstream os;expr;if (!os) throw std::runtime_error("MAKESTRINGOS " MAKESTR_FILE_LINE ": failed to write ");string=os.str();}while(0)
+// usage: MAKESTRS(str,os << 1;os << 2) => str="12"
+#define MAKESTRS(string,expr) do {std::ostringstream os;expr;if (!os) throw std::runtime_error(MAKESTR_FILE_LINE ": failed to write" #expr);string=os.str();}while(0)
 // usage: MAKESTR(str,1 << 2) => str="12"
 #define MAKESTR(string,expr) MAKESTRS(string,os << expr)
 #define MAKESTRFL(string,expr) MAKESTR(string,MAKESTR_FILE_LINE << expr)
+
+//DOESN'T WORK!
+//#define MAKESTRE(expr) static_cast<std::ostringstream &>((*(std::ostringstream *)&std::ostringstream()) << "" <<  expr).str()
+//#define MAKECSTRE(expr) MAKESTRE(expr).c_str()
 
 // (obviously, no comma/newlines allowed in expr)
 
@@ -34,11 +38,18 @@ BOOST_AUTO_UNIT_TEST( makestring )
     MAKESTRS(s,os << "12");BOOST_CHECK(s==e);
     MAKESTRS(s,os << 1 << 2);BOOST_CHECK(s==e);
     MAKESTRS(s,os << 1;os << 2);BOOST_CHECK(s==e);
-    
+
     //copy of MAKESTRS
     MAKESTR(s,"12");BOOST_CHECK(s==e);
     MAKESTR(s,1 << 2);BOOST_CHECK(s==e);
     MAKESTR(s,1;os << 2);BOOST_CHECK(s==e);
+
+    //copy of MAKESTRS
+//     BOOST_CHECK(MAKESTRE("12")==e);
+//     BOOST_CHECK(MAKESTRE(1 << 2)==e);
+//     BOOST_CHECK(MAKECSTRE('1' << '2')==e);
+//     BOOST_CHECK(MAKECSTRE(1 << 2)==e);
+   
 #line 42
     MAKESTRFL(s,"asdf");BOOST_CHECK(s=="(makestr.hpp:42)asdf");
     
