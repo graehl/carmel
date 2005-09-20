70f6a429ff1a956d2dc6662983ac39ea409532c9
Jonathan Graehl <graehl@gmail.com>
2005-09-20 22:32:52 +0000

    *** empty log message ***
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@900 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/Debug.h
+++ shared/Debug.h
@@ -36,6 +36,7 @@ using namespace std;
 #define assertlvl(level,assertion) IF_ASSERT(level) {assert(assertion);}
 
 // from makestr.hpp (GraehlCVS)
+#if 0
 #define MAKESTR_STRINGIZE(str) #str
 #define MAKESTR_EXPAND_STRINGIZE(expr) MAKESTR_STRINGIZE(expr)
 #define MAKESTR_FILE_LINE "(" __FILE__  ":" MAKESTR_EXPAND_STRINGIZE(__LINE__)  ")"
@@ -46,7 +47,10 @@ using namespace std;
 // usage: MAKESTR(str,1 << 2) => str="12"
 #define MAKESTR(string,expr) MAKESTRS(string,os << expr)
 #define MAKESTRFL(string,expr) MAKESTR(string,MAKESTR_FILE_LINE << expr)
+#define 
+#endif
 
+#include "makestr.hpp"
 
 #ifdef NO_INFO
 # define DBG_OP_F_NL(lvl,pDbg,op,module,msg,file,line,newline)
@@ -57,12 +61,11 @@ using namespace std;
 
 # define DBG_OP_F_NL(lvl,pDbg,op,module,oexp,file,line,newline) do {     \
         if (INFO_LEVEL >= lvl && (pDbg)->runtime_info_level >= lvl) {   \
-   ostringstream o; \
+   ostringstream os; \
    oexp; \
-   if(!o) throw std::runtime_error(MAKESTR_FILE_LINE ": failed to write " #module " : " #msg); \
-   (pDbg)->op(module,o.str(),file,line,newline);      \
+   if(!os) throw std::runtime_error(MAKESTR_FILE_LINE ": failed to write " #module " : " #oexp); \
+   (pDbg)->op(module,os.str(),file,line,newline);      \
 } } while(0)
-
 #endif
 
 //!< Q: no FILE/LINE included in output
@@ -74,13 +77,15 @@ using namespace std;
 #define DBG_OP_LQ(lvl,pDbg,op,module,msg) DBG_OP_F(lvl,pDbg,op,module,msg,"",0)
 #define NESTINFO_GUARD(pDbg,lvl) ns_decoder_global::Debug::Nest debug_nest_guard_ ## __LINE__ (pDbg,lvl)
 
+#define O_INSERT(msg) os << msg
+
 // some of these names might be used, e.g. in windows.h, but seems ok on linux.  compilation will fail/warn if they are in conflict.
 #define NESTINFO NESTINFO_GUARD(dbg,1)
-#define INFO(module,msg) DBG_OP(dbg,info,module,o<<msg)
-#define WARNING(module,msg) DBG_OP(dbg,warning,module,o<<msg)
-#define ERROR(module,msg) DBG_OP(dbg,error,module,o<<msg)
-#define FATAL(module,msg) DBG_OP(dbg,fatalError,module,o<<msg)
-#define INFOQ(module,msg) DBG_OP_Q(dbg,info,module,o<<msg)
+#define INFO(module,msg) DBG_OP(dbg,info,module,O_INSERT(msg))
+#define WARNING(module,msg) DBG_OP(dbg,warning,module,O_INSERT(msg))
+#define ERROR(module,msg) DBG_OP(dbg,error,module,O_INSERT(msg))
+#define FATAL(module,msg) DBG_OP(dbg,fatalError,module,O_INSERT(msg))
+#define INFOQ(module,msg) DBG_OP_Q(dbg,info,module,O_INSERT(msg))
 #define INFOB(module) dbg->info_begin(module,__FILE__,__LINE__)
 #define WARNINGB(module) dbg->warning_begin(module,__FILE__,__LINE__)
 #define ERRORB(module) dbg->error_begin(module,__FILE__,__LINE__)
@@ -89,48 +94,48 @@ using namespace std;
 #define WARNINGBQ(module) dbg->warning_begin(module,__FILE__,0)
 #define ERRORBQ(module) dbg->error_begin(module,__FILE__,0)
 
-#define INFOQSAMELINE(module,msg) DBG_OP_LQ_NEWLINE(0,dbg,info,module,o<<msg,false)
+#define INFOQSAMELINE(module,msg) DBG_OP_LQ_NEWLINE(0,dbg,info,module,O_INSERT(msg),false)
 #define INFOSTREAM dbg->info_sameline()
 #define INFOSTREAM_NL dbg->info_startline()
-#define WARNINGLQ(lvl,module,msg) DBG_OP_LQ(lvl,dbg,warning,module,o<<msg)
-#define WARNINGL(lvl,module,msg) DBG_OP_L(lvl,dbg,warning,module,o<<msg)
-#define WARNINGQ(module,msg) DBG_OP_Q(dbg,warning,module,o<<msg)
-#define ERRORQ(module,msg) DBG_OP_Q(dbg,error,module,o<<msg)
-#define FATALQ(module,msg) DBG_OP_Q(dbg,fatalError,module,o<<msg)
-#define INFOLQ(lvl,module,msg) DBG_OP_LQ(lvl,dbg,info,module,o<<msg)
+#define WARNINGLQ(lvl,module,msg) DBG_OP_LQ(lvl,dbg,warning,module,O_INSERT(msg))
+#define WARNINGL(lvl,module,msg) DBG_OP_L(lvl,dbg,warning,module,O_INSERT(msg))
+#define WARNINGQ(module,msg) DBG_OP_Q(dbg,warning,module,O_INSERT(msg))
+#define ERRORQ(module,msg) DBG_OP_Q(dbg,error,module,O_INSERT(msg))
+#define FATALQ(module,msg) DBG_OP_Q(dbg,fatalError,module,O_INSERT(msg))
+#define INFOLQ(lvl,module,msg) DBG_OP_LQ(lvl,dbg,info,module,O_INSERT(msg))
 #define INFOLQE(lvl,module,oexp) DBG_OP_LQ(lvl,dbg,info,module,oexp)
-#define INFOL(lvl,module,msg) DBG_OP_L(lvl,dbg,info,module,o<<msg)
+#define INFOL(lvl,module,msg) DBG_OP_L(lvl,dbg,info,module,O_INSERT(msg))
 #if INFO_LEVEL >=9
-#define INF9(module,msg) INFOLQ(9,module,o<<msg)
+#define INF9(module,msg) INFOLQ(9,module,O_INSERT(msg))
 #define INF9IN dbg->increase_depth()
 #define INF9OUT dbg->decrease_depth()
 #define INF9NEST NESTINFO_GUARD(dbg,9)
 #else
-#define INF9(module,o<<msg)
+#define INF9(module,msg)
 #define INF9IN
 #define INF9OUT
 #define INF9NEST
 #endif
 
 #if INFO_LEVEL >=99
-#define INF99(module,msg) INFOLQ(99,module,o<<msg)
+#define INF99(module,msg) INFOLQ(99,module,O_INSERT(msg))
 #define INF99IN dbg->increase_depth()
 #define INF99OUT dbg->decrease_depth()
 #define INF99NEST NESTINFO_GUARD(dbg,99)
 #else
-#define INF99(module,o<<msg)
+#define INF99(module,msg)
 #define INF99IN
 #define INF99OUT
 #define INF99NEST
 #endif
 
 #if (defined(TEST) && !defined(QUIET_TEST) )
-#define INFOT(msg) DBG_OP(&test_dbg,info,"TEST",o<<msg)
-#define WARNT(msg) DBG_OP(&test_dbg,warning,"TEST",o<<msg)
+#define INFOT(msg) DBG_OP(&test_dbg,info,"TEST",O_INSERT(msg))
+#define WARNT(msg) DBG_OP(&test_dbg,warning,"TEST",O_INSERT(msg))
 #define NESTT NESTINFO_GUARD(&test_dbg)
 #else
-#define INFOT(msg) DBG_OP_L(99,dbg,info,"TEST",o<<msg)
-#define WARNT(msg) DBG_OP_L(99,dbg,warning,"TEST",o<<msg)
+#define INFOT(msg) DBG_OP_L(99,dbg,info,"TEST",O_INSERT(msg))
+#define WARNT(msg) DBG_OP_L(99,dbg,warning,"TEST",O_INSERT(msg))
 #if INFO_LEVEL >= 99
 #define NESTT NESTINFO_GUARD(dbg,99)
 #else
