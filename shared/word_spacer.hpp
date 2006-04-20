62ffb211bac99a0d88ffec31f2530f10a4d3de55
Jonathan Graehl <graehl@gmail.com>
2006-04-20 22:09:30 +0000

    new print tree,string,derivation,foreign, renamed token_type -> token_type_id due to conflict with nested token_type -> indexed_token alias, defined top_token_text string constant for TOP
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@972 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/word_spacer.hpp
@@ -0,0 +1,98 @@
+#ifndef GRAEHL__SHARED__WORD_SPACER_HPP
+#define GRAEHL__SHARED__WORD_SPACER_HPP
+
+#include <sstream>
+#include <string>
+
+namespace graehl {
+/// print spaces between each word (none before or after)
+///
+///usage: word_spacer sp; while(...) o << sp << ...;
+///
+/// alternative: word_spacer_sp; while(...) o << get_string() << ...;
+struct word_spacer {
+    bool first;
+    char space_string[2];
+    word_spacer(char space=' ') : first(true) { space_string[0]=space;space_string[1]=0;}
+    const char *empty() const
+    {
+        return space_string+1;
+    }
+    const char *space() const
+    {
+        return space_string;
+    }
+    const char *get_string() 
+    {
+        if (first) {
+            first=false;
+            return empty();
+        } else {
+            return space();
+        }
+    }
+    void reset() 
+    {
+        first=true;
+    }
+    template <class O>
+    void print(O &o)
+    {
+        if (first)
+            first=false;
+        else
+            o << space();
+    }    
+    template <class O> friend inline O& operator<<(O& o,word_spacer &me) 
+    {
+        me.print(o);
+        return o;
+    }
+};
+
+inline std::string space_sep_words(const std::string &sentence,char space=' ')
+{
+    using namespace std;
+    stringstream o;
+    string word;
+    istringstream i(sentence);
+    word_spacer sep(space);
+    while (i >> word)
+        o << sep << word;
+    return o.str();
+}
+
+inline int compare_space_normalized(const std::string &a, const std::string &b)
+{
+    return space_sep_words(a).compare(space_sep_words(b));
+}
+
+
+//!< print before word.
+template <char sep=' '>
+struct WordSeparator {
+    bool first;
+    WordSeparator() : first(true) {}
+    void reset() 
+    {
+        first=true;
+    }
+    template <class O>
+    void print(O&o) 
+    {
+        if (first)
+            first=false;
+        else
+            o << sep;
+    }
+    typedef WordSeparator<sep> Self;
+    static const char seperator=sep;
+    template <class O> friend inline O& operator<<(O& o,Self &me) 
+    {
+        me.print(o);
+        return o;
+    }
+};
+
+}
+#endif
