5a3ad9d5e0f6086fe907eb86a6f1293cbec90afb
Jonathan Graehl <graehl@gmail.com>
2006-06-01 18:10:09 +0000

    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1059 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/null_terminated.hpp
@@ -0,0 +1,81 @@
+#ifndef GRAEHL__SHARED__NULL_TERMINATED_HPP
+#define GRAEHL__SHARED__NULL_TERMINATED_HPP
+
+#include <boost/iterator/iterator_facade.hpp>
+#include <cstring>
+
+template <class C> inline
+bool is_null(C const &c) 
+{ return !c; }
+
+template <class C> inline
+void set_null(C &c)
+{ c=C(); }
+
+
+    
+
+template <class C>
+class null_terminated_iterator :
+    public boost::iterator_facade<
+        null_terminated_iterator<C>,
+        C,
+        boost::bidirectional_traversal_tag>
+{
+ public:
+    
+    // end is default constructed
+    null_terminated_iterator(C *p=0) : p(p) {}
+    
+    static inline null_terminated_iterator<C> end()
+    { return null_terminated_iterator<C>(); }
+    
+ private:
+    C *p;
+    
+    friend class boost::iterator_core_access;
+
+    void increment()
+    {
+        if (is_null(++p))
+            p=0;
+    }
+    
+    C& dereference() const { return *p; }
+    
+    bool equal(null_terminated_iterator<C> const& other) const
+    {
+        return p==other.p;
+    }
+};  
+
+typedef null_terminated_iterator<char> cstr_iterator;
+typedef null_terminated_iterator<char const> const_cstr_iterator;
+
+inline char const* null_terminated_end(char const* s)
+{
+    return s + std::strlen(s);
+}
+
+template <class C>
+inline C const* null_terminated_end(C const* s)
+{
+    while (*s++) ;
+    return s;
+}
+
+//nonconst (copies of above)
+inline char * null_terminated_end(char * s)
+{
+    return s + std::strlen(s);
+}
+
+template <class C>
+inline C * null_terminated_end(C * s)
+{
+    while (*s++) ;
+    return s;
+}
+
+
+#endif
