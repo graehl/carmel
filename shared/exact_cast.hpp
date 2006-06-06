efd918ced6005b613d5498d09d3a98356290d29c
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2006-06-06 19:22:39 +0000

    exact_static_cast checks for underflow at runtime
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1065 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/exact_cast.hpp
@@ -0,0 +1,33 @@
+#ifndef GRAEHL__SHARED__EXACT_CAST_HPP
+#define GRAEHL__SHARED__EXACT_CAST_HPP
+
+#include <stdexcept>
+
+namespace graehl {
+
+struct inexact_cast : public std::runtime_error 
+{
+    inexact_cast() : std::runtime_error("inexact_cast - casting to a different type lost information") {}
+};
+
+template <class To,class From>
+To exact_static_assign(To &to,From const& from) 
+{
+    to=static_cast<To>(from);
+    if (static_cast<From>(to)!=from)
+        throw inexact_cast();
+}
+
+template <class To,class From>
+To exact_static_cast(From const& from) 
+{
+    To to;
+    exact_static_assign(to,from);
+    return to;
+}
+
+
+
+}//graehl
+
+#endif
