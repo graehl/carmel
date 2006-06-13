a5ef327a2e29fa99a77b09135df2ab7f324c71c7
Jonathan Graehl <graehl@gmail.com>
2006-06-13 21:36:08 +0000

    fixed adjust_for_retry (added print to verify parameters used).  --retry-curvature only affects histograms, though (check math for score_t beams)
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1076 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/time_series.hpp
+++ shared/time_series.hpp
@@ -1,11 +1,14 @@
 #ifndef GRAEHL__SHARED__TIME_SERIES_HPP
 #define GRAEHL__SHARED__TIME_SERIES_HPP
 
+//FIXME: curvature does nothing if <=0 for score_t, otherwise results in +INF (bug) if positive
+
 #ifdef SAMPLE
 //# define DEBUG_TIME_SERIES
 #endif
 
 #ifdef DEBUG_TIME_SERIES
+#define GRAEHL__DEBUG_PRINT
 # include <graehl/shared/debugprint.hpp>
 #endif 
 #include <cstddef>
@@ -25,12 +28,15 @@ struct time_series
 };
 */
 
-/// Returns must support: Returns=pow(Value,double),+, -,*,/. 
-template <class Returns=double>
+/// Computes must support: Computes=pow(Value,double),+, -,*,/.
+ /// adaptable unary function: time_type(=double) -> Returns
+template <class Computes=double,class Returns=Computes>
 struct clamped_time_series : public std::unary_function<double,Returns>
 {
-    typedef Returns value_type;
+    typedef Computes value_type;
+    typedef Returns return_type;
     typedef double time_type;
+    typedef time_type first_argument_type;
     value_type x0;
     value_type k;
     value_type x_origin;
@@ -47,6 +53,9 @@ struct clamped_time_series : public std::unary_function<double,Returns>
     // duration <= 0 -> constant series (=start)
     void set(value_type start,value_type end,time_type duration,double curvature=0)
     {
+#ifdef DEBUG_TIME_SERIES
+        DBP4(start,end,duration,curvature);
+#endif
         if (duration <= 0) { //set constant fn
             k=1;
             t_max=1;
@@ -57,7 +66,7 @@ struct clamped_time_series : public std::unary_function<double,Returns>
         t_max=duration;
         value_type xN;
         if (curvature) {
-            x_origin=static_cast<value_type>(curvature*end);
+            x_origin=(end*static_cast<value_type>(curvature));
             x0=start-x_origin;
             xN=end-x_origin;
         } else {
@@ -77,16 +86,21 @@ struct clamped_time_series : public std::unary_function<double,Returns>
     value_type end() const
     {
         return x0*k+x_origin;
-    }   
+    }
 
-    value_type operator()(time_type t) const
+    value_type value(time_type t) const
     {
         if (t<=0)
             return start();
         else if (t>=t_max)
             return end();
         else
-            return static_cast<value_type>(x0*pow(k,t/t_max)+x_origin);
+            return x0*pow(k,t/t_max)+x_origin;
+    }
+    
+    return_type operator()(time_type t) const
+    {
+        return static_cast<return_type>(value(t));
     }
 
 };
@@ -104,6 +118,8 @@ int main(int argc, char *argv[])
     using namespace std;
     using namespace graehl;
     double s=8,e=2,t_max=3;
+    {
+        
     clamped_time_series<double>
         s1(s,e,t_max,0),
         s2(s,e,t_max,.9),
@@ -111,7 +127,21 @@ int main(int argc, char *argv[])
     for (double t=0;t<=t_max+1;t+=.5) {
         cout << "t="<<t<<" "<<s1(t);
         cout <<" "<<s2(t)<<" "<<s3(t)<<endl;
+    }
+    }
+    {
+        
+    clamped_time_series<double,unsigned>
+        s1(s,e,t_max,0),
+        s2(s,e,t_max,.9),
+        s3(s,e,t_max,-1e5);
+    for (double t=0;t<=t_max+1;t+=.5) {
+        cout << "t="<<t<<" "<<s1(t);
+        cout <<" "<<s2(t)<<" "<<s3(t)<<endl;
     }   
+    
+    }
+    
     return 0;
 }
 
