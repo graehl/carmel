1bd0c62abeac498d54329a650e55452d80f278ea
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2006-06-27 23:45:37 +0000

    fixed: override --weight-string by --weight-lm-ngram
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1123 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/is_null.hpp
+++ shared/is_null.hpp
@@ -1,6 +1,8 @@
 #ifndef GRAEHL__SHARED__IS_NULL_HPP
 #define GRAEHL__SHARED__IS_NULL_HPP
 
+#include <cmath>
+
 template <class C> inline
 bool is_null(C const &c) 
 { return !c; }
@@ -8,6 +10,29 @@ bool is_null(C const &c)
 template <class C> inline
 void set_null(C &c)
 { c=C(); }
+
+inline bool is_null(float const& f)
+{
+    return std::isnan(f);//f!=f;
+//    return f!=f;
+}
+
+inline void set_null(float &f)
+{
+    f=NAN;//0./0.;
+}
+
+inline bool is_null(double const& f)
+{
+    return std::isnan(f);
+// return f!=f;
+}
+
+inline void set_null(double &f)
+{
+    f=NAN;//0./0.;
+}
+
 
 
 #endif
