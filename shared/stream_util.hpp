15c7ef5a4d6863cca553be5f2a3925d782a6b471
Jonathan Graehl <graehl@gmail.com>
2006-06-30 22:41:42 +0000

    set precision only locally for size_mega
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1143 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/stream_util.hpp
+++ shared/stream_util.hpp
@@ -3,6 +3,7 @@
 
 #include <iomanip>
 #include <iostream>
+#include <cmath>
 
 #define TO_OSTREAM_PRINT                                                                     \
     template <class C,class T> \
@@ -39,6 +40,100 @@ struct local_stream_flags
         pstream->flags(saved_flags);
     }
 };
+
+template <class Stream>
+struct local_precision
+{
+    typedef Stream stream_type;
+    stream_type *pstream;
+    std::streamsize saved_prec;
+    local_precision(stream_type &stream,unsigned prec) : pstream(&stream), saved_prec(stream.precision(prec)) {}
+    local_precision(stream_type &stream) : pstream(&stream), saved_prec(stream.precision()) {}
+    ~local_precision() 
+    {
+        pstream->precision(saved_prec);
+    }
+};
+
+template <class O>
+struct local_stream_format
+{
+    local_stream_flags<O> f;
+    local_precision<O> p;
+};
+
+    
+
+template <class O> inline
+O & print_max_width(O&o,double d,int width=6) 
+{
+    local_stream_format<O> save(o);
+    if (width > 0) {
+        double p=std::fabs(d);
+        if (d<0)
+            --width;
+        int wholes=std::log10(p)+1; //1: log=0, digits=1
+        unsigned need=(wholes < 0) ? -wholes+1 : wholes;
+        if (need > width) {
+            unsigned unit_e_exp=1+1+2;
+            if (width >= unit_e_exp)
+                o << std::scientific << std::setprecision(width-unit_e_exp);
+        } else {
+            unsigned need_dot=need+1;
+            if (need_dot < width)
+                o << std::setprecision(width-need_dot);
+            else
+                o << std::setprecision(0);
+        }
+    }
+    return o << d;
+}
+
+template <class O> inline
+O & print_max_width_small(O&o,double d,int width=4) 
+{
+    local_precision<O> save(o);
+    int p=-1;
+    if (width>0) {    
+        if (d>=0 && d< 10000) {
+            if (d < 10) {
+                p=width-2;
+            } else if (d<100) {
+                p=width-3;
+            } else
+                p=0;
+        }
+    }
+    if (p>0)
+        o << std::setprecision(p);
+    return o << d;
+}
+
+#if 0
+std::ostream &trunc_(std::ostream &st, const char *s)
+{
+    if (st.opfx()) {
+        int l = strlen(s), w = st.width(0);
+        if (l > w)
+            if (st.flags() & ios::right)
+                st.write(s + l - w, w);
+            else
+                st.write(s, w);
+        else
+            st << s;
+        st.osfx();
+    }
+    return st;
+}
+
+
+/* //omanip = old gcc extension?
+omanip<const char *> trunc(const char *s)
+{
+    return omanip<const char *>(trunc_, s);
+} 
+*/
+#endif
 
 }//graehl
 
