cceb05277e5b1aeac660111b61d2ea8dd4f0ca61
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2007-02-02 02:37:33 +0000

    -0 -> null output stream (not segfault if used without checking)
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1423 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/null_ostream.hpp
+++ shared/null_ostream.hpp
@@ -3,29 +3,32 @@
 
 #include <streambuf>
 #include <ostream>
-
-#ifdef GRAEHL__SINGLE_MAIN
-# define GRAEHL__NULL_OSTREAM_MAIN
-#endif 
+#include <string>
 
 namespace graehl {
 
-class null_streambuf : public std::streambuf
+template <class C, class T = std::char_traits<C> >
+class basic_null_streambuf : public std::basic_streambuf<C,T>
 {
-    virtual int overflow(int c) { return std::char_traits< char >::not_eof(c); }
+    virtual int overflow(typename T::int_type  c) { return T::not_eof(c); }
 public:
-    null_streambuf() {}
+    basic_null_streambuf() {}
 };
 
-# ifdef GRAEHL__NULL_OSTREAM_MAIN
-null_streambuf null_streambuf_instance;
-std::ostream null_ostream(&null_streambuf_instance);
-# else
-extern null_streambuf null_streambuf_instance;
-extern std::ostream null_ostream;
-# endif
+typedef basic_null_streambuf<char> null_streambuf;
 
-# endif 
+template <class C, class T = std::char_traits<C> >
+class basic_null_ostream: public std::basic_ostream<C,T>
+{
+    basic_null_streambuf<C,T> nullbuf;
+ public:
+    basic_null_ostream() :
+        std::basic_ios<C,T>(&nullbuf),
+        std::basic_ostream<C,T>(&nullbuf)
+    { init(&nullbuf); }
+};
+
+typedef basic_null_ostream<char> null_ostream;
 
 }//graehl
 
