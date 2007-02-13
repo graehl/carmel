13940bab23ad0dd8697943e5d6b7ceaaab7ea354
Jonathan Graehl <graehl@gmail.com>
2007-02-13 09:28:33 +0000

    compile fix
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1442 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/bit_arithmetic.hpp
+++ shared/bit_arithmetic.hpp
@@ -40,7 +40,7 @@ inline uint16_t unpack_uint16_little(void const*d)
 //FIXME: test if the #ifdef optimization is even needed (compiler may optimize portable to same?)
 #if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
   || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
-return *((const uint16_t *) (d)));
+return *((const uint16_t *) (d));
 #else
 return ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
         +(uint32_t)(((const uint8_t *)(d))[0]) );
