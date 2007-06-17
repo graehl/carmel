81c59358d7f6bd64da8a9ad2a74e5a803825cf73
Jonathan Graehl <graehl@gmail.com>
2007-06-17 07:11:21 +0000

    carmel locked arc weight input error fixed
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1675 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- carmel/test/traintest.sh
+++ carmel/test/traintest.sh
@@ -1,4 +1,4 @@
-cmd="$B -o 1.1 -M 3 -F span.spell.trained2 -t span.spell.corpus span.spell.wfst"
+cmd="$B -o 1.1 -M 10 -F span.spell.trained2 -t span.spell.corpus span.spell.wfst"
 #$B -o 1.1 -M 3 -F span.spell.trained2 -t span.spell.corpus span.spell.wfst
 echo $cmd
 $cmd
