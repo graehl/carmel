24fe0c6492f191bec6cc3746471af9cf2dddb668
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2007-08-08 22:23:07 +0000

    thread synchronization for --cached-ngram-size N.  report collisions
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@1722 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/dynamic_hash_cache.hpp
+++ shared/dynamic_hash_cache.hpp
@@ -4,33 +4,81 @@
 #include <graehl/shared/dynamic_sized.hpp>
 #include <graehl/shared/doubling_primes.hpp>
 #include <graehl/shared/hash_functions.hpp>
+#include <graehl/shared/no_locking.hpp>
 #include <cassert>
+#include <iostream>
+
+// coarse grain locking (saves memory, negligible when you can just use a smaller cache, but worse performance).  note: no space or time overhead with graehl::no_locking
+//#define DYNAMIC_HASH_CACHE_SINGLE_LOCK
+
+#define DYNAMIC_HASH_CACHE_TRACK_COLLISIONS
 
 namespace graehl {
 
 // all this essentially so I can keep unsigned *p, unsigned len separate rather than bundled into a struct?  seems silly but it's done.
+/*
+  struct Backing {
+ typedef unsigned result_type;
+ result_type operator()(unsigned *p,unsigned len);
+}
+*/
 
-// void Backing::get(unsigned *p,unsigned len,result_type *r)
-// note: key is pascal_words_string, hash/equal/assign is hard coded for pod of lenx32bits pod
-template <class result_type,class Backing>
+// note: key is stored as pascal_words_string, hash/equal/assign is hard coded plain-old-binary-data
+template <class Backing,class Locking=graehl::no_locking>
 struct dynamic_hash_cache
+#ifdef DYNAMIC_HASH_CACHE_SINGLE_LOCK
+    : private Locking
+#endif 
 {
+    typedef typename Backing::result_type result_type;
+    typedef Locking locking_type;
+    typedef typename locking_type::scoped_lock lock_type;
+#ifdef DYNAMIC_HASH_CACHE_TRACK_COLLISIONS
+    std::size_t n_collide;
+#endif 
     typedef unsigned const* quad_p;
-    
-    typedef dynamic_hash_cache<result_type,Backing> self_type;
+    struct locked_result
+#ifndef DYNAMIC_HASH_CACHE_SINGLE_LOCK
+    : public locking_type
+#endif
+    {
+        result_type result;
+    };
+
+    locking_type &locking(locked_result &r)
+    {
+        return
+#ifdef DYNAMIC_HASH_CACHE_SINGLE_LOCK
+            *this
+#else
+            r
+#endif
+            ;
+    }
+    typedef dynamic_hash_cache<Backing,Locking> self_type;
     typedef graehl::pascal_words_string key_type;
-    typedef graehl::dynamic_pair<result_type,key_type> cache_entry;
+    typedef graehl::dynamic_pair<locked_result,key_type> cache_entry;
     unsigned cachesize;
     unsigned max_len;
     dynamic_sized_array<cache_entry> cache;
     Backing b;
     void flush()
     {
-        n_miss=n_hit=0;
+#ifdef DYNAMIC_HASH_CACHE_SINGLE_LOCK
+        lock_type l(*this);
+#endif 
+#ifdef DYNAMIC_HASH_CACHE_TRACK_COLLISIONS
+        n_collide=
+#endif 
+            n_miss=n_hit=0;
+        
         assert(cachesize==cache.size);
         for (unsigned i=0;i<cachesize;++i) {
-            pascal_words_string&p=cache[i].dynamic;
-            p.set_null();
+            cache_entry &e=cache[i];
+#ifndef DYNAMIC_HASH_CACHE_SINGLE_LOCK
+            lock_type l(e.fixed);
+#endif 
+            e.dynamic.set_null();
         }
     }
 
@@ -38,12 +86,28 @@ struct dynamic_hash_cache
     {
         return cachesize;
     }
+
+    template <class O>
+    void stats(O &o) const
+    {
+        
+        o << " (cache capacity="<< capacity()<<" hit rate="<<std::setprecision(2)<<hit_rate()*100<<"% hits="<<n_hit<<" misses="<<n_miss;
+#ifdef DYNAMIC_HASH_CACHE_TRACK_COLLISIONS
+        o << " hash collisions="<<n_collide<< " collision rate="<<std::setprecision(2)<<100.*n_collide/n_queries();
+#endif 
+        o <<")";
+    }
     
     std::size_t n_miss,n_hit;
+    double n_queries() const 
+    {
+        return (double)n_miss+(double)n_hit;
+    }
+    
     // returns [0...1] portion of hits
     double hit_rate() const
     {
-        return n_hit / ((double)n_miss+n_hit);
+        return n_hit / n_queries();
     }
     dynamic_hash_cache(Backing const& b,unsigned max_len,unsigned cache_size=10000)
         : 
@@ -56,27 +120,42 @@ struct dynamic_hash_cache
     }
 
     // conceptually const: answer doesn't depend on whether cache was modified
-    result_type &operator()(quad_p p,unsigned len) const 
+    result_type operator()(quad_p p,unsigned len) const 
     {
         return const_cast<self_type&>(*this)(p,len);
     }
     
-    result_type &operator()(quad_p p,unsigned len)
+    result_type operator()(quad_p p,unsigned len)
     {
         assert(len <= max_len);
         assert(cachesize==cache.size);
         unsigned i=(unsigned)graehl::hash_quads_64(p,len) % cachesize;
+
+        bool hit;        
         cache_entry &e=cache[i];
-        result_type &cached_val=e.fixed;
         key_type &cached_key=e.dynamic;
-        if (cached_key.equal(p,len)) {
-            ++n_hit;
-        } else {
-            cached_key.set(p,len);
-            ++n_miss;
-            b.get(p,len,&cached_val);
+        locking_type &m=locking(e.fixed);
+        result_type &cached_result=e.fixed.result;
+        {
+            lock_type l(m);
+            hit=cached_key.equal(p,len);
+            if (hit) {
+                ++n_hit;
+                return cached_result;
+            }
+#ifdef DYNAMIC_HASH_CACHE_TRACK_COLLISIONS
+            if (!cached_key.is_null())
+                ++n_collide;
+#endif 
         }
-        return cached_val;
+        ++n_miss;
+        result_type r=b(p,len);
+        {    
+            lock_type l(m);
+            cached_key.set(p,len);
+            cached_result=r;
+        }        
+        return r;
     }
     
 };
