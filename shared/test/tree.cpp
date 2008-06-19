71b06abbdea10228c9ea9eada494ed04729b1123
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2008-06-19 00:38:31 +0000

    tree memleak fixed
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2106 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ shared/test/tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define MAIN
+//#define SINGLE_PRECISION
+#define DOUBLE_PRECISION
+
+#include <graehl/shared/tree.hpp>
+#include <graehl/shared/string_to.hpp>
+
+int main(int argc, char *argv[])
+{
+    using namespace graehl;
+    using namespace std;
+    
+    if (argc<2) {
+        cerr<<"argument: tree with int labels";
+        return -1;
+    }
+    tree<int> t;
+    std::string s(argv[1]);
+    string_into(argv[1],t);
+    cout << t << "\n";
+    return 0;        
+}
