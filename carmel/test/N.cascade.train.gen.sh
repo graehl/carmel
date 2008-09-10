4e59a03003eca170da584d0eafc9e89a78f1948c
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2008-09-10 00:47:57 +0000

    carmel 4.1 test cases
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2198 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ carmel/test/N.cascade.train.gen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+carmel=${carmel:-carmel}
+N=${N:-100}
+M=${M:-5}
+function safefilename {
+   echo $* | perl -pe 's/\W+/./g'
+}
+comp=comp.`safefilename $*`
+corp=.corpus.$comp.$N
+$carmel $* > $comp
+$carmel -g $N $comp > $corp
+uchain=
+for f in $*; do
+ $carmel -n --constant-weight=1 $f > $f.u
+ uchain="$uchain $f.u"
+done
+$carmel -S $corp $uchain >/dev/null
+$carmel -S $corp $comp >/dev/null
+$carmel -M $M --train-cascade $ARGS $corp $uchain
+for f in $*; do
+ echo original:
+ $carmel $f
+ echo trained:
+ $carmel $f.u.trained
+done
