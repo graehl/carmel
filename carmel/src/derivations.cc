e5cc3e9c4a6549994455392139231c6a126c00c5
Jonathan Graehl <graehl@gmail.com>
2009-08-20 18:52:12 +0000

    ws
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2410 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- carmel/src/derivations.cc
+++ carmel/src/derivations.cc
@@ -2,29 +2,27 @@
 
 namespace graehl {
 
-derivations::state_id derivations::derive(wfst_io_index const& io,deriv_state const& d) 
+derivations::state_id derivations::derive(wfst_io_index const& io,deriv_state const& d)
 {
     const int EPS=WFST::epsilon_index;
     state_id ret=g.size();
-    
+
     state_to_id::insert_return_type already=
         id_of_state.insert(d,ret);
     if (!already.second)
         return already.first->second;
 
-    
     add(id_of_state,d,ret); // NOTE: very important that we've added this before we start taking self-epsilons.
     g.push_back();
-        
+
     wfst_io_index::for_state const&fs=io.st[d.s];
-        
+
     add_arcs(io,EPS,EPS,d.i,d.o,fs,ret);
-    
-    
+
     if (d.o < out.size()) {
         add_arcs(io,EPS,out[d.o],d.i,d.o+1,fs,ret);
     }
-    
+
     if (d.i < in.size()) {
         Sym i=in[d.i];
         add_arcs(io,i,EPS,d.i+1,d.o,fs,ret);
@@ -33,7 +31,7 @@ derivations::state_id derivations::derive(wfst_io_index const& io,deriv_state co
         }
     }
 
-    return ret;    
+    return ret;
 }
 
 
