bbafbe8e3b2f4c91e7c25c14cc88ae6b86d71e5d
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2009-08-20 18:52:12 +0000

    ws
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2410 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- carmel/src/derivations.h
+++ carmel/src/derivations.h
@@ -3,11 +3,11 @@
 
 #ifdef DEBUGDERIVATIONS
 # define DEBUG_DERIVATIONS_PRUNE
-#endif 
+#endif
 
 /*
 
-special form of compose: input string * transducer * output string (more efficient than compose.h (input string * transducer) * output string.  
+special form of compose: input string * transducer * output string (more efficient than compose.h (input string * transducer) * output string.
 */
 #include <boost/utility.hpp>
 #include <graehl/shared/myassert.h>
@@ -26,10 +26,10 @@ special form of compose: input string * transducer * output string (more efficie
 
 namespace graehl {
 
-struct deriv_state 
+struct deriv_state
 {
     uint32_t i,s,o; // input,state,output
-    std::size_t hash() const 
+    std::size_t hash() const
     {
         return hash_quads_64(&i,sizeof(deriv_state)/sizeof(i));
 //hash_bytes_32((void *)this,sizeof(deriv_state));
@@ -54,7 +54,7 @@ struct arcs_table : public dynamic_array<arc_counts>
         n_states=x.numStates();
         x.visit_arcs(*this);
     }
-    void operator()(unsigned s,FSTArc &a) 
+    void operator()(unsigned s,FSTArc &a)
     {
         this->push_back();
         arc_counts &ac=this->back();
@@ -64,19 +64,19 @@ struct arcs_table : public dynamic_array<arc_counts>
             ac.prior_counts+=a.weight;
         ac.src=s;
     }
-    
+
 #define ARCS_TABLE_EACH(i) for (arcs_type::iterator i=this->begin(),end=this->end();i!=end;++i)
 #define ARCS_TABLE_EACH_C(i) for (arcs_type::const_iterator i=this->begin(),end=this->end();i!=end;++i)
 
     template <class V>
-    void visit(V &v) 
+    void visit(V &v)
     {
         ARCS_TABLE_EACH(i)
             v(*i);
     }
 
     template <class V>
-    void visit(V const& v) 
+    void visit(V const& v)
     {
         ARCS_TABLE_EACH(i)
             v(*i);
@@ -94,19 +94,19 @@ struct arcs_table : public dynamic_array<arc_counts>
     {
         ARCS_TABLE_EACH_C(i)
             v(*i);
-    }    
-    
-    void dump(std::ostream &o,char const* header="") const 
+    }
+
+    void dump(std::ostream &o,char const* header="") const
     {
         o << "\n" << header << "\n";
         ARCS_TABLE_EACH_C(i)
             o << *i;
     }
-    
+
 #undef ARCS_TABLE_EACH_C
 #undef ARCS_TABLE_EACH
 };
-    
+
 struct wfst_io_index : boost::noncopyable
 {
     typedef dynamic_array<unsigned> for_io; // id in arcs_table
@@ -115,7 +115,7 @@ struct wfst_io_index : boost::noncopyable
 
     states_t st;
     arcs_table &t;
-        
+
     wfst_io_index(arcs_table &t) : st(t.n_states),t(t)
     {
         for (unsigned i=0,N=t.size();i!=N;++i) {
@@ -135,36 +135,36 @@ struct derivations //: boost::noncopyable
 //    typedef List<int> Symbols;
     typedef int Sym;
     typedef dynamic_array<Sym> Seq;
-    
+
     Seq in,out; // cleared after compute()
-    
+
     typedef dynamic_array<GraphState> vgraph;
     vgraph g;
     typedef unsigned state_id;
     state_id fin;
     bool no_goal;
     bool cache_backward;
-    
+
     typedef HashTable<deriv_state,state_id> state_to_id;
     state_to_id id_of_state;
  public:
-    struct statistics 
+    struct statistics
     {
-        struct states_arcs 
+        struct states_arcs
         {
             double states,arcs;
             states_arcs() {
                 clear();
             }
-            void clear() 
+            void clear()
             {
                 states=arcs=0;
             }
-            void print(std::ostream &o) const 
+            void print(std::ostream &o) const
             {
                 o << "(" << states << " states, "<<arcs<<" arcs)";
             }
-            void operator /= (states_arcs const& d) 
+            void operator /= (states_arcs const& d)
             {
                 states/=d.states;
                 if (d.arcs==0)
@@ -175,15 +175,14 @@ struct derivations //: boost::noncopyable
             typedef states_arcs self_type;
             TO_OSTREAM_PRINT
         };
-        
 
         states_arcs pre,post;
-        
-        void print(std::ostream &o) const 
+
+        void print(std::ostream &o) const
         {
             states_arcs ratio=post;
             ratio/=pre;
-            
+
             o << "\nFor all cached derivations:\n"
               <<  "Pre pruning: "<< pre <<"\n"
               <<"Post pruning: " << post << "\n"
@@ -195,27 +194,27 @@ struct derivations //: boost::noncopyable
     };
 
     static statistics global_stats;
-    
+
     double weight;
     unsigned lineno;
-    
-    bool empty() const 
+
+    bool empty() const
     {
         return no_goal;
     }
-    state_id final() const 
+    state_id final() const
     {
         assert(!empty());
         return fin;
     }
 
-    unsigned n_deriv_fsa_states() const 
+    unsigned n_deriv_fsa_states() const
     {
         return g.size();
     }
 
     /*
-    void update_weights(arcs_table &t) 
+    void update_weights(arcs_table &t)
     {
         for (vgraph::iterator i=g.begin(),e=g.end();i!=e;++i) {
             unsigned arcid=i->data_as<unsigned>();
@@ -224,15 +223,15 @@ struct derivations //: boost::noncopyable
     }
     */
 
-    unsigned n_states() const 
+    unsigned n_states() const
     {
         return g.size();
     }
-    
+
     Graph graph() const
     {
         assert(!empty());
-        
+
         Graph r;
         r.states=const_cast<GraphState *>(&g.front());
         r.nStates=g.size();
@@ -243,12 +242,12 @@ struct derivations //: boost::noncopyable
 
     typedef fixed_array<Weight> fb_weights;
 
-    struct weight_for 
+    struct weight_for
     {
         typedef Weight result_type;
         arcs_table const& t;
         weight_for(arcs_table const& t) : t(t) {}
-        arc_counts &ac(GraphArc const& a) const 
+        arc_counts &ac(GraphArc const& a) const
         {
             assert(a.data_as<unsigned>()<t.size());
             return t[a.data_as<unsigned>()];
@@ -257,38 +256,38 @@ struct derivations //: boost::noncopyable
         {
             return ac(a).weight();
         }
-    };    
+    };
 
-    state_id start() const 
+    state_id start() const
     {
         return 0;
     }
-    
+
     Weight collect_counts(arcs_table &t)
     {
 //        update_weights(t);
         weight_for wf(t);
-        
+
         unsigned nst=g.size();
         fb_weights f(nst),b(nst); // default 0-init
 
 
         f[0]=1;
         get_order();
-        
-        propagate_paths_in_order(graph(),reverse_order.rbegin(),reverse_order.rend(),wf,f);        
+
+        propagate_paths_in_order(graph(),reverse_order.rbegin(),reverse_order.rend(),wf,f);
         Weight prob=f[fin];
-        
+
 //        reversed_graph r(g); // NOTE: we could cache this reversed graph as well, but we'd run into swapping sooner.  I bet it's faster to recompute for each example by a lot when you get to that size range, and not much slower before.
         get_reverse();
-        
+
 
         b[fin]=1;
         propagate_paths_in_order(r.graph(),reverse_order.begin(),reverse_order.end(),wf,b);
 
         free_order();
         free_reverse();
-        
+
         check_fb_agree(prob,b[0]);
 
         for (unsigned s=0;s<nst;++s) {
@@ -298,10 +297,9 @@ struct derivations //: boost::noncopyable
                 arc_counts &ac=wf.ac(a);
                 Weight arc_contrib=ac.weight()*f[a.src]*b[a.dest];
                 ac.counts += arc_contrib*weight/prob;
-            }            
+            }
         }
 
-        
         return prob;
     }
  private:
@@ -322,14 +320,14 @@ struct derivations //: boost::noncopyable
     }
 
     template <class Symbols>
-    bool init_and_compute(WFST &x,wfst_io_index const& io,Symbols const& in_,Symbols const& out_,double w=1,unsigned line=0,bool cache_backward_=false,bool drop_names=true,bool prune_=true) 
+    bool init_and_compute(WFST &x,wfst_io_index const& io,Symbols const& in_,Symbols const& out_,double w=1,unsigned line=0,bool cache_backward_=false,bool drop_names=true,bool prune_=true)
     {
         init(in_,out_,w,line,cache_backward_);
         return compute(x,io,drop_names,prune_);
     }
-    
-    bool compute(WFST &x,wfst_io_index const& io,bool drop_names=true,bool prune_=true) 
-    {   
+
+    bool compute(WFST &x,wfst_io_index const& io,bool drop_names=true,bool prune_=true)
+    {
         state_id start=derive(io,deriv_state(0,0,0));
         assert(start==0);
         deriv_state goal(in.size(),x.final,out.size());
@@ -358,7 +356,7 @@ struct derivations //: boost::noncopyable
 
     // nonportable serialization to temporary rewindable tape file
     template <class A>
-    void serialize(A &a) 
+    void serialize(A &a)
     {
         a & fin & g & weight & lineno;
         if (A::is_loading) {
@@ -375,14 +373,14 @@ struct derivations //: boost::noncopyable
         in.clear();
         out.clear();
     }
-    
+
     typedef GraphState::arcs_type arcs_type;
 
-    struct reversed_graph 
+    struct reversed_graph
     {
         fixed_array<GraphState> b;
-        reversed_graph() {}        
-        
+        reversed_graph() {}
+
         template <class Graph>
         reversed_graph(Graph const& forward)
         {
@@ -397,11 +395,11 @@ struct derivations //: boost::noncopyable
             add_reversed_arcs(&b.front(),&forward.front(),forward.size());
         }
 
-        void clear() 
+        void clear()
         {
             b.clear();
         }
-        
+
         /// m[i] is set to val if state leads to i along a path in b, the reversed graph (i.e. in the orig. graph, something leads to "state".  you must set all m[i] !=val if they haven't already been visited by this
         template <class Mark,class Val>
         void mark_reaching(Mark &m,unsigned state,Val v) const
@@ -420,13 +418,10 @@ struct derivations //: boost::noncopyable
             ret.nStates=b.size();
             return ret;
         }
-        
+
     };
-    
-        
-    
-    
-    void prune() 
+
+    void prune()
     {
         if (empty())
             return;
@@ -434,18 +429,18 @@ struct derivations //: boost::noncopyable
 #ifdef DEBUG_DERIVATIONS_PRUNE
         Config::debug() << "Pruning derivations - original:\n";
         printGraph(graph(),Config::debug());
-#endif        
+#endif
         reversed_graph r(g);
 #ifdef DEBUG_DERIVATIONS_PRUNE
         Config::debug() << "Pruning derivations - reversed:\n";
         printGraph(r.graph(),Config::debug());
-#endif        
+#endif
         r.mark_reaching(remove,final(),false);
 #ifdef DEBUG_DERIVATIONS_PRUNE
         Config::debug() << "Removing states:";
         print_range(Config::debug(),remove.begin(),remove.end());
         Config::debug() << "\n";
-#endif  
+#endif
         indices_after_removing ttable(remove);
         fin=ttable[fin];
         // now: remove[i] = true -> no path to final
@@ -457,19 +452,19 @@ struct derivations //: boost::noncopyable
         Config::debug()<<"old state->new state ttable:\n";
         print_range(Config::debug(),ttable.map,ttable.map+ttable.n_mapped);
         Config::debug()<<"\n";
-                
+
         Config::debug() << "Pruning derivations - final, new size="<<new_size<<":\n";
-                
+
         printGraph(graph(),Config::debug());
-#endif        
+#endif
         g.resize(new_size);
     }
-    
+
  private:
     state_id derive (wfst_io_index const& io,deriv_state const& d);
-    
+
     void add_arcs(wfst_io_index const& io,Sym s_in,Sym s_out,unsigned i_in,unsigned i_out
-                  ,wfst_io_index::for_state const& fs,unsigned source) 
+                  ,wfst_io_index::for_state const& fs,unsigned source)
     {
         typedef wfst_io_index::for_io for_io;
         if (for_io const* match=find_second(fs,IOPair(s_in,s_out)))
@@ -480,14 +475,14 @@ struct derivations //: boost::noncopyable
                 g[source].add_data_as(source,derive(io,deriv_state(i_in,a.dest(),i_out)),0,id);
 // note: had to use g[source] rather than caching the iterator, because recursion may invalidate any previously taken iterator
             }
-        
+
     }
-    
+
     reversed_graph r;
     dynamic_array<unsigned> reverse_order;
     // because of pruning, there is not much gain in visiting from the final state in reversed order.  the reverse of reverse_order is the forward order starting at start (0).  and the reverse of that is a valid order for starting at finish and following reverse arcs (it's just that finish may not be the first thing on the reverse_order, if pruning didn't happen, or if there were cycles
-    
-    void make_reverse() 
+
+    void make_reverse()
     {
         r.init(g);
     }
@@ -524,12 +519,8 @@ struct derivations //: boost::noncopyable
     }
 };
 
-
 }
 
 BEGIN_HASH_VAL(graehl::deriv_state) {	return x.hash(); } END_HASH
-
-
-
 
 #endif
