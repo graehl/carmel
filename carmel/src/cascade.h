6d97a33016f7c14cea39aef45ebbf9742794bda0
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2009-09-16 06:44:15 +0000

    carm
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2422 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- carmel/src/cascade.h
+++ carmel/src/cascade.h
@@ -35,14 +35,13 @@ struct cascade_parameters
     chain_id nil_chain;
     unsigned debug; //bitfield
     enum { DEBUG_CHAINS=1,DEBUG_CASCADE=2,DEBUG_COMPOSED=4,DEBUG_COMPRESS=8,DEBUG_COMPRESS_VERBOSE=16 };
-        
-    
+
     typedef HashTable<param,chain_id> epsilon_map_t; // any pair of arcs from a*b will only occur once in composition, but a single epsilon a or b may reoccur many times. we want to use a single chain_id for all those, so we have to hash
 
     epsilon_map_t epsilon_chains;
 
     typedef WFST::saved_weights_t saved_weights_t;
-    
+
     void save_weights(WFST const&composed,saved_weights_t &save) const
     {
         if (trivial) {
@@ -54,7 +53,7 @@ struct cascade_parameters
             (*i)->save_weights(save);
     }
 
-    void restore_weights(WFST & composed,saved_weights_t const& save) 
+    void restore_weights(WFST & composed,saved_weights_t const& save)
     {
         if (trivial) {
             composed.restore_weights(save.begin());
@@ -67,42 +66,42 @@ struct cascade_parameters
         update(composed); // note: don't need to normalize
         //FIXME: is that update redundant in practice?
     }
-    
-    
-    void clear_counts() 
+
+
+    void clear_counts()
     {
         for (cascade_t::iterator i=cascade.begin(),e=cascade.end();
              i!=e;++i)
             (*i)->zero_arcs(); // skips actual locked arcs
     }
 
-    void distribute_chain_counts(chain_t p,Weight counts) 
+    void distribute_chain_counts(chain_t p,Weight counts)
     {
         for (;p;p=p->next)
             distribute_counts(*p->data,counts);
     }
 
-    void distribute_counts(FSTArc &a,Weight counts) 
+    void distribute_counts(FSTArc &a,Weight counts)
     {
         if (!a.isLocked()) // because locked arcs w/ weight other than 1 need to appear in chain, but can't have their weights altered
             a.weight += counts;
     }
-    
+
 
     void distribute_chain_id_counts(chain_id id,Weight counts)
     {
         assert(id>=0 && id<chains.size());             // note: by construction, a cascade-composed fst will have no locked arcs; rather, it will refer to a chain which references a locked arc
         distribute_chain_counts(chains[id],counts);
     }
-    
+
     struct arcs_table_distribute_counts
     {
         cascade_parameters &cascade;
         arcs_table_distribute_counts(cascade_parameters &cascade) : cascade(cascade) {}
-        void operator()(unsigned /*source*/,FSTArc const&a) const 
+        void operator()(unsigned /*source*/,FSTArc const&a) const
         {
             // note: using weight which is, after prep_new_weights, including the global prior. //FIXME: per-arc prior in original transducers also
-            cascade.distribute_chain_id_counts(a.groupId,a.weight);   
+            cascade.distribute_chain_id_counts(a.groupId,a.weight);
         }
     };
 
@@ -128,53 +127,53 @@ struct cascade_parameters
         update(composed);
     }
 
-    void set_trivial() 
+    void set_trivial()
     {
         chain_weights.clear();
         chains.clear();
         epsilon_chains.clear();
         trivial=true;
     }
-    
+
     cascade_parameters(bool remember_cascade=false,unsigned debug=0)
         : debug(debug)
     {
         if ((trivial=!remember_cascade)) return;
 
         //chains.push_back(0); // we don't mind using a 0 index since we don't use groupids at all when cascade composing
-        
+
         nil_chain=chains.size();
         chains.push_back(0); // canonical nil index for compositions  where every parameter was locked with weight of 1.
         // note composition will create locked -> final state arcs for epsilon filter finals.  but locked_group is 0, so you get nil_chain anyway
         assert(nil_chain==FSTArc::locked_group);
-        
+
         // empty chain means: don't update the original arc in any way
     }
 
-    void normalize(WFST &composed,WFST::NormalizeMethod const& method) 
+    void normalize(WFST &composed,WFST::NormalizeMethod const& method)
     {
         if (trivial)
             composed.normalize(method);
         else
             normalize(method);
     }
-    
-    void normalize(WFST::NormalizeMethod const& method) 
+
+    void normalize(WFST::NormalizeMethod const& method)
     {
         for (cascade_t::iterator i=cascade.begin(),e=cascade.end();
              i!=e;++i)
             (*i)->normalize(method);
     }
 
-    
+
     void randomize()
     {
         for (cascade_t::iterator i=cascade.begin(),e=cascade.end();
              i!=e;++i)
             (*i)->randomSet();
-    }    
-    
-    void normalize_and_update(WFST &composed,WFST::NormalizeMethod const& method) 
+    }
+
+    void normalize_and_update(WFST &composed,WFST::NormalizeMethod const& method)
     {
         normalize(composed,method);
         update(composed);
@@ -189,20 +188,20 @@ struct cascade_parameters
             randomize();
             //normalize_and_update(composed,method);
             normalize(method); // update happens at top of random restarts loop already
-        }    
+        }
     }
-    
-    
-    
+
+
+
     // not to be used on arcs that weren't composed via cascade
-    void update_composed_arc(FSTArc &comp) 
+    void update_composed_arc(FSTArc &comp)
     {
-//        assert(!comp.isLocked());        
+//        assert(!comp.isLocked());
         assert(comp.groupId < chain_weights.size());
         comp.weight=chain_weights[comp.groupId];
     }
 
-    void accumulate_chain(Weight &w,chain_t p) 
+    void accumulate_chain(Weight &w,chain_t p)
     {
         for (;p;p=p->next)
             w *= p->data->weight;
@@ -212,14 +211,14 @@ struct cascade_parameters
     void calculate_chain_weights()
     {
         chain_weights.clear();
-        chain_weights.resize(chains.size(),Weight::ONE()); 
+        chain_weights.resize(chains.size(),Weight::ONE());
         for (unsigned i=0,e=chains.size();
              i!=e;++i) {
             accumulate_chain(chain_weights[i],chains[i]); // recall, nil chain will keep the default ONE as it's an empty list
-        }   
+        }
     }
 
-    void print(std::ostream &o,bool cascade=true,bool chains=true) 
+    void print(std::ostream &o,bool cascade=true,bool chains=true)
     {
         if (cascade)
             print_cascade(o);
@@ -227,14 +226,14 @@ struct cascade_parameters
             print_chains(o);
     }
 
-    void print_cascade(std::ostream &o) 
+    void print_cascade(std::ostream &o)
     {
         for (unsigned i=0,e=cascade.size();
              i!=e;++i)
             o << "cascade["<<i<<"]:\n"<<*cascade[i]<<"\n";
     }
-    
-    void print_chains(std::ostream &o,bool weights=true) 
+
+    void print_chains(std::ostream &o,bool weights=true)
     {
         o << "Composed chains:\n";
         for (unsigned i=0,e=chains.size();
@@ -251,7 +250,7 @@ struct cascade_parameters
         }
     };
 
-    void print_chain(std::ostream &o,unsigned i,bool weights=true) 
+    void print_chain(std::ostream &o,unsigned i,bool weights=true)
     {
         o<<i<<": \t";
         if (weights)
@@ -260,15 +259,15 @@ struct cascade_parameters
         c.print_writer(o,param_writer());
         o << "\n";
     }
-    
-    
-    void update(WFST &composed) 
+
+
+    void update(WFST &composed)
     {
         if (trivial) return;
         if (debug&DEBUG_COMPOSED)
             Config::debug() << "composed pre:\n" << composed << std::endl;
         // composed has groupids that are indices into chains, unless trivial
-        calculate_chain_weights();        
+        calculate_chain_weights();
         for (WFST::StateVector::iterator i=composed.states.begin(),e=composed.states.end();i!=e;++i) {
             State::Arcs &arcs=i->arcs;
             for ( State::Arcs::val_iterator l=arcs.val_begin(),end=arcs.val_end() ; l != end ; ++l )
@@ -281,12 +280,12 @@ struct cascade_parameters
 
     bool is_chain[2]; // is_chain[second] tells if arcs given to record are already chained, i.e. should you cons then append or just append
 
-    void prepare_compose() 
+    void prepare_compose()
     {
         prepare_compose(false,false);
     }
-    
-    
+
+
     void prepare_compose(bool right_assoc) // shorthand for left->right.  right_assoc -> first is not chain, second is
     {
         if (right_assoc)
@@ -294,31 +293,31 @@ struct cascade_parameters
         else
             prepare_compose(true,false);
     }
-    
-    void prepare_compose(bool first_chain,bool second_chain) 
+
+    void prepare_compose(bool first_chain,bool second_chain)
     {
         is_chain[0]=first_chain;
         is_chain[1]=second_chain;
     }
-    
-    
-    chain_id record1(FSTArc const* e) 
+
+
+    chain_id record1(FSTArc const* e)
     {
         return record_eps(const_cast<param>(e),is_chain[0]);
     }
 
-    chain_id record2(FSTArc const* e) 
+    chain_id record2(FSTArc const* e)
     {
         return record_eps(const_cast<param>(e),is_chain[1]);
     }
-    
-    inline chain_t cons(param a,chain_t cdr=0) 
+
+    inline chain_t cons(param a,chain_t cdr=0)
     {
         if (is_locked_1(a)) return cdr;
         return pool.construct(a,cdr);
     }
 
-    inline chain_t cons(param a,param b) 
+    inline chain_t cons(param a,param b)
     {
         return cons(a,cons(b));
     }
@@ -337,7 +336,7 @@ struct cascade_parameters
         return b;
     }
 
-    chain_t cons_chain(param a,param b) 
+    chain_t cons_chain(param a,param b)
     {
         assert(!is_chain[0] || a->groupId <= chains.size());
         assert(!is_chain[1] || b->groupId <= chains.size());
@@ -354,29 +353,29 @@ struct cascade_parameters
                 return cons(a,b);
         }
     }
-    
-    chain_id record(FSTArc const* a,FSTArc const* b) 
+
+    chain_id record(FSTArc const* a,FSTArc const* b)
     {
         return record(const_cast<param>(a),const_cast<param>(b));
     }
 
-    static inline bool is_locked_1(param e) 
+    static inline bool is_locked_1(param e)
     {
         return e->isLocked() && e->weight.isOne();
     }
 
-    chain_id locked_1_groupid() 
+    chain_id locked_1_groupid()
     {
         return trivial?WFST::locked_group:nil_chain;
     }
-    
+
     inline chain_id original_id(param e)
     {
         return is_locked_1(e) ? nil_chain : e->groupId;
     }
-    
+
     //if the entire (e) or (a,b) is all is_locked_1(), then undo push_back to vector and reference a canonical 'nil' chain_t  index
-    chain_id record_eps(param e,bool chain=false) 
+    chain_id record_eps(param e,bool chain=false)
     {
         if (trivial) return e->groupId; // for -a composition, but also means epsilons with tie groups in regular composition maintain group
         if (chain) return original_id(e);
@@ -390,7 +389,7 @@ struct cascade_parameters
         return ins.first->second;
     }
 
-    chain_id record(param a,param b) 
+    chain_id record(param a,param b)
     {
         if (trivial) return FSTArc::no_group;
         // a * b should be generated at most once, so add it immediately
@@ -414,7 +413,7 @@ struct cascade_parameters
             remove[nil_chain]=false;
         }
 
-        void find_used(WFST const& w) 
+        void find_used(WFST const& w)
         {
             w.visit_arcs_sourceless(*this);
             newids.init(remove);
@@ -422,11 +421,11 @@ struct cascade_parameters
         }
 
         // from above find_used
-        void operator()(FSTArc const& a) 
+        void operator()(FSTArc const& a)
         {
             if (a.isLocked()) return;
             remove[a.groupId]=false;
-        }        
+        }
 
         void rewrite_arcs(WFST & w)
         {
@@ -434,16 +433,16 @@ struct cascade_parameters
         }
 
         // from above rewrite_arcs_using
-        void operator()(unsigned s,FSTArc & a) 
+        void operator()(unsigned s,FSTArc & a)
         {
             if (a.isLocked()) return;
             assert(!newids.removing(a.groupId));
             a.groupId=newids[a.groupId];
-        }   
+        }
     };
 
     // call *before* calculate_chain_weights (weights aren't moved also)
-    void compress_chains(WFST &composed) 
+    void compress_chains(WFST &composed)
     {
         bool v=DEBUG_COMPRESS_VERBOSE&debug;
         bool d=DEBUG_COMPRESS&debug;
@@ -456,7 +455,7 @@ struct cascade_parameters
         debug_chains(d,"compress chains post",v);
     }
 
-    void debug_chains(bool enable=true,char const* header="chains",bool verbose=true) 
+    void debug_chains(bool enable=true,char const* header="chains",bool verbose=true)
     {
         if (enable) {
             Config::debug()<<"\n"<<header;
@@ -471,7 +470,7 @@ struct cascade_parameters
         }
     }
 
-    void done_composing(WFST &composed,bool compress_removed_arcs=false) 
+    void done_composing(WFST &composed,bool compress_removed_arcs=false)
     {
         if (trivial) return;
         epsilon_chains.clear();
@@ -479,12 +478,12 @@ struct cascade_parameters
             compress_chains(composed);
     }
 
-    void add(WFST *w) 
+    void add(WFST *w)
     {
         if (trivial) return;
         cascade.push_back(w);
     }
-    
+
 };
 
 }//ns
