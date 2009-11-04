graehl/carmel

commit 84ea5988510a6a590dd65eb9ec610fbaed890736

--normby=N (None) and -M 0 (frac count only)
git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2534 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

carmel/src/derivations.h
@@ -550,7 +550,7 @@ struct derivations //: boost::noncopyable
     {
         if (empty())
             return;
-        /*
+#if 1
         fixed_array<bool> remove(true,g.size());
 #ifdef DEBUG_DERIVATIONS_PRUNE
         Config::debug() << "Pruning derivations - original:\n";
@@ -562,7 +562,7 @@ struct derivations //: boost::noncopyable
         printGraph(r.graph(),Config::debug());
 #endif
         r.mark_reaching(remove,final(),false);
-        */
+#endif
 #ifdef DEBUG_DERIVATIONS_PRUNE
         Config::debug() << "Removing states:";
         print_range(Config::debug(),remove.begin(),remove.end());
@@ -615,15 +615,15 @@ struct derivations //: boost::noncopyable
         typename wfst_io_index::for_state const&fs=io.st[d.s];
         add_arcs(io,atab,EPS,EPS,d.i,d.o,fs,src);
         bool bad=!is_goal(d);
-        bool useO=d.o<out.size();
-        if (useO) {
-            bad&=add_arcs(io,atab,EPS,out[d.o],d.i,d.o+1,fs,src);
-        }
-        if (d.i < in.size()) {
+        bool useO=d.o<out.size(),useI=d.i<in.size();
+        unsigned o1=d.o+1,i1=d.o+1;
+        if (useO)
+            bad&=add_arcs(io,atab,EPS,out[d.o],d.i,o1,fs,src);
+        if (useI) {
             Sym si=in[d.i];
-            bad&=add_arcs(io,atab,si,EPS,d.i+1,d.o,fs,src);
+            bad&=add_arcs(io,atab,si,EPS,i1,d.o,fs,src);
             if (useO) {
-                bad&=add_arcs(io,atab,si,out[d.o],d.i+1,d.o+1,fs,src);
+                bad&=add_arcs(io,atab,si,out[d.o],i1,o1,fs,src);
             }
         }
         remove[src]=bad;
@@ -643,7 +643,7 @@ struct derivations //: boost::noncopyable
                 ++global_stats.pre.arcs;
                 FSTArc *a=atab[id].arc;
                 unsigned dst=derive(io,atab,deriv_state(i_in,a->dest,i_out));
-                if (true||!remove[dst]) {
+                if (true||!remove[dst]) { //FIXME: remove array isn't showing reachability to final (goal) state, so disabled.
                     bad=false;
                     g[source].add_data_as(source,dst,a->weight.getReal(),id); // weight only used by gibbs init em prob
 // note: had to use g[source] rather than caching the iterator, because recursion may invalidate any previously taken iterator

carmel/src/train.cc
@@ -525,6 +525,16 @@ Weight WFST::train(cascade_parameters &cascade,
     random_restart_acceptor ra=opts.ra;
 
     forward_backward fb(*this,cascade,weight_is_prior_count,smoothFloor,true,opts,corpus);
+    Weight corpus_p;
+    if (opts.max_iter==0) {
+        Config::log() << "0 iterations specified for training; output weights will be unnormalized fractional counts (except locked arcs).\n";
+        Weight p=fb.estimate(corpus_p);
+        corpus_p.print_ppx(Config::log(),corpus.n_output,corpus.n_pairs); //FIXME: newPerplexity is training-example-weighted
+        fb.arcs.visit(for_arcs::prep_new_weights(1.0));
+        cascade.use_counts(*this,methods);
+        Config::log()<<"\n";
+        return p.ppxper(corpus.totalEmpiricalWeight);
+    }
 
     Weight bestPerplexity;
     bestPerplexity.setInfinity();
@@ -564,7 +574,6 @@ Weight WFST::train(cascade_parameters &cascade,
 #endif
 
 //            DWSTAT("Before estimate");
-            Weight corpus_p;
             bool cascade_counts=using_cascade && !first_time;
 
             if (cascade_counts) {
@@ -910,6 +919,8 @@ Weight forward_backward::maximize(WFST::NormalizeMethods const& methods,FLOAT_TY
 
     DUMPDW("Weights before prior smoothing");
 
+    cascade.save_none(methods);
+
     //    arcs.pre_norm_counts(corpus.totalEmpiricalWeight);
     arcs.visit(for_arcs::prep_new_weights(1.0));
 
@@ -918,6 +929,8 @@ Weight forward_backward::maximize(WFST::NormalizeMethods const& methods,FLOAT_TY
 
 //    DWSTAT("Before normalize");
     cascade.use_counts(x,methods); // doesn't actually put weights back into x for nontrivial cascade, which is why the following is skipped for cascades.  update prior to estimate puts the weights in place.
+
+    cascade.load_none(methods);
 
     if (cascade.trivial) {
         DUMPDW("Weights after normalization");

carmel/src/cascade.h
@@ -151,6 +151,30 @@ struct cascade_parameters
         composed.visit_arcs(v);
     }
 
+    dynamic_array<WFST::saved_weights_t> none_saves;
+
+    void save_none(WFST::NormalizeMethods const& methods)
+    {
+        unsigned N=methods.size();
+        assert(N==cascade.size());
+        none_saves.reinit(N);
+        for (unsigned i=0;i!=N;++i)
+            if (methods[i].group==WFST::NONE)
+                cascade[i]->save_weights(none_saves[i]);
+    }
+
+    void load_none(WFST::NormalizeMethods const& methods)
+    {
+        unsigned N=methods.size();
+        assert(N==cascade.size());
+        for (unsigned i=0;i!=N;++i)
+            if (methods[i].group==WFST::NONE) {
+                cascade[i]->restore_weights(none_saves[i]);
+                none_saves[i].clear();
+            }
+    }
+
+
     void use_counts(WFST &composed,WFST::NormalizeMethods const& methods)
     {
         distribute_counts(composed);

commit 82aaa9129df777f96ab035e8027a06840427fae0

fixed typo in derivations introduced in r2534
git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2545 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

carmel/src/carmel.cc
@@ -344,6 +344,7 @@ struct carmel_main
     unsigned n_prob;
     unsigned n_lines() const { return n_0prob+n_prob; }
     Weight prod_sum_pre; // for post-b
+//    double n_isymbols;
     double n_symbols;
 
     void non0_viterbi_prob(Weight p)
@@ -535,6 +536,7 @@ struct carmel_main
         n_0prob=0;
         n_prob=0;
         n_symbols=0; // for per-symbol ppx - counted only for non-0prob
+//        n_isymbols=0;
         prod_viterbi=1;
         prod_sum=1;
         prod_sum_pre=1;

carmel/src/derivations.h
@@ -672,7 +672,7 @@ struct derivations //: boost::noncopyable
         bool dead=(d!=goal);
         if (add_arcs(io,atab,EPS,EPS,d.i,d.o,fs,src)) dead=false;
         bool useO=d.o<out.size(),useI=d.i<in.size();
-        unsigned o1=d.o+1,i1=d.o+1;
+        unsigned o1=d.o+1,i1=d.i+1;
         if (useO)
             if (add_arcs(io,atab,EPS,out[d.o],d.i,o1,fs,src)) dead=false;
         if (useI) {

carmel/src/train.cc
@@ -12,10 +12,20 @@
 
 namespace graehl {
 
+
 void training_progress(unsigned train_example_no,unsigned scale,unsigned num_every)
 {
     num_progress(Config::log(),train_example_no,scale,num_every,".","\n");
 }
+
+void training_progress_scale(unsigned n,unsigned N,unsigned num_every)
+{
+    unsigned d=2*num_every;
+    unsigned s=(n+d-1)/d;
+    if (s<1) s=1;
+    training_progress(n,s,num_every);
+}
+
 
 derivations::statistics derivations::global_stats;
 
@@ -366,7 +376,7 @@ struct forward_backward : public cached_derivs<arc_counts>
  public:
     void operator()(unsigned n,derivations &derivs)
     {
-        training_progress(n);
+        training_progress_scale(n,corpus().size());
         Weight prob=derivs.collect_counts(arcs);
         *unweighted_corpus_prob*=prob;
         weighted_corpus_prob*=prob.pow(derivs.weight);
@@ -531,7 +541,7 @@ Weight WFST::train(cascade_parameters &cascade,
     if (opts.max_iter==0) {
         Config::log() << "0 iterations specified for training; output weights will be unnormalized fractional counts (except locked arcs).\n";
         Weight p=fb.estimate(corpus_p);
-        corpus_p.print_ppx(Config::log(),corpus.n_output,corpus.n_pairs); //FIXME: newPerplexity is training-example-weighted
+        corpus_p.print_ppx_symbol(Config::log(),corpus.n_input,corpus.n_output,corpus.n_pairs); //FIXME: newPerplexity is training-example-weighted
         fb.arcs.visit(for_arcs::prep_new_weights(1.0));
         cascade.use_counts(*this,methods);
         Config::log()<<"\n";
@@ -593,7 +603,7 @@ Weight WFST::train(cascade_parameters &cascade,
             DWSTAT("\nAfter estimate");
             Config::log() << "i=" << train_iter << " (rate=" << learning_rate << "): ";
 //            Config::log() << " per-output-symbol-perplexity="<<corpus_p.ppxper(corpus.n_output).as_base(2)<<" per-example-perplexity="<<newPerplexity.as_base(2);
-            corpus_p.print_ppx(Config::log(),corpus.n_output,corpus.n_pairs); //FIXME: newPerplexity is training-example-weighted
+            corpus_p.print_ppx_symbol(Config::log(),corpus.n_input,corpus.n_output,corpus.n_pairs); //FIXME: newPerplexity is training-example-weighted
             if ( newPerplexity < bestPerplexity && (!using_cascade || cascade_counts)) { // because of how I'm saving only composed counts, we can't actually get back to our initial starting point (iter 1)
                 Config::log() << " (new best)";
                 bestPerplexity=newPerplexity;
@@ -818,7 +828,7 @@ Weight forward_backward::estimate_matrix(Weight &unweighted_corpus_prob)
 
         //#ifdef DEBUGTRAIN // Yaser 13-7-2000 - Debugging messages ..
         ++train_example_no ;
-        training_progress(train_example_no);
+        training_progress(train_example_no,corpus().size());
         //#endif
         nIn = seq->i.n;
         nOut = seq->o.n;

carmel/src/train.h
@@ -12,7 +12,7 @@ namespace graehl {
 void check_fb_agree(Weight f,Weight b);
 
 void training_progress(unsigned train_example_no,unsigned scale=10,unsigned num_every=70);
-
+void training_progress_scale(unsigned n,unsigned N,unsigned num_every=70); //
 
 struct arc_counts_base
 {