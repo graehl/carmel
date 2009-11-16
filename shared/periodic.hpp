753f658006f2f2cee532d89fdbaceac387a73919
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2009-11-16 22:58:57 +0000

    forest-em --crp viterbi
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2570 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/periodic.hpp
+++ shared/periodic.hpp
@@ -107,25 +107,35 @@ struct num_tick_writer
 typedef periodic_wrapper_i<num_tick_writer> num_ticker;
 
 template <class O>
+void num_tick(O &o,unsigned i,char const* tick=".",char const* post_num="",char const* pre_num="",bool tick_and_num=false)
+{
+    o << pre_num << i << post_num;
+    if (tick_and_num)
+        o << tick;
+}
+
+template <class O>
 void num_progress(O &o,unsigned i,unsigned tick_every=10,unsigned num_every_ticks=70,char const* tick=".",char const* post_num="",char const* pre_num="",bool tick_and_num=false,bool num_0=true)
 {
     unsigned num_every=tick_every*num_every_ticks;
-    if (i % num_every == 0 && (i>0||num_0)) {
-        o << pre_num << i << post_num;
-        if (tick_and_num)
-            o << tick;
-    } else if (i % tick_every == 0)
+    if (i % num_every == 0 && (i>0||num_0))
+        num_tick(o,i,tick,post_num,pre_num,tick_and_num);
+    else if (i % tick_every == 0)
         o << tick;
 }
 
 
+// end with n=N if you want final number always displayed (i.e. recommend n=1 as first element of N)
 template <class O>
-void num_progress_scale(O &o,unsigned n,unsigned N,unsigned num_every=70,char const* tick=".",char const* post_num="\n",char const* pre_num="",bool tick_and_num=false,bool num_0=true)
+void num_progress_scale(O &o,unsigned n,unsigned N,unsigned num_every=70,unsigned n_nums=2,char const* tick=".",char const* post_num="\n ",char const* pre_num="",bool tick_and_num=false,bool num_0=true,bool final_num=true)
 {
-    unsigned d=2*num_every;
+    unsigned d=n_nums*num_every;
     unsigned s=(N+d-1)/d;
     if (s<1) s=1;
-    num_progress(o,n,s,num_every,tick,post_num,pre_num,tick_and_num,num_0);
+    if (n==N)
+        num_tick(o,n,tick,post_num,pre_num,tick_and_num);
+    else
+        num_progress(o,n,s,num_every,tick,post_num,pre_num,tick_and_num,num_0);
 }
 
 }
