b3e06ec4938af7da0003cc93eee6ac37df12b2df
Jonathan Graehl <graehl@gmail.com>
2009-12-08 21:03:41 +0000

    clm.
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2634 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- clm/extract.clm.sh
+++ clm/extract.clm.sh
@@ -1,13 +1,18 @@
-rm x.*
+#!/bin/bash
+ix=${ix:-training}
+ox=${ox:-x}
 set -x
-extract "$@" -w x.left -W x.right -s 0 -e ${head:=10} -N ${N:=3} -r training -x x.rules -G - -g 1 -l 1000:4 -m 5000 -O -T -i -X
+extract "$@" -w $ox.left -W $ox.right -s 0 -e ${head:-999999999} -N ${N:=3} -r $ix -x ${ruleout:=/dev/null} -G - -g 1 -l 1000:${bign:=0} -m 5000 -O -T -i -X
 set +x
 set -e
 function one {
     perl -pe 's/$/ 1/' "$@"
 }
+function bocounts {
+    perl -ne '@a=split;for (0..$#a) { print join(" ",@a[$_..$#a]),"\n" }' "$@"
+}
 function filt {
-    egrep '^[0-9]' -- "$@" | cut  -f4- | one
+    egrep '^[0-9]' -- "$@" | cut  -f4- | bocounts | one
 }
 function show {
     echo '==>' $name '<=='
@@ -15,7 +20,7 @@ function show {
     head "$@"
 }
 function Evocab {
-    perl -ne '$e{$2}=1 if /(^| )(E\S+)/;END{print "$_\n" for (keys %e)}' "$@"
+    perl -ne '$e{$2}=1 while /(^| )(E\S+)/g;END{print "$_\n" for (keys %e)}' "$@"
 }
 function clm_from_counts {
     local count=${1:?'Ea Eb Fc x' e.g. x=1 time, clm ngram counts.  E... are all nonevents (context), F... is predicted}
@@ -23,22 +28,27 @@ function clm_from_counts {
     local out=$1
     shift
     name=Evocab ngram=${N:-3}
-    local Ev=`mktemp`
+    local Ev=$count.Ev
+    #`mktemp`
     Evocab $count > $Ev
     show $count $Ev
     echo using ngram order N=$ngram
     local unkargs="-unk"
     local ngoargs="-order $ngram"
-    local smoothargs="-wbdiscount"
+    local noprune="-minprune $((ngram+1))"
+    local smoothargs="-cdiscount 0"
+    #"-wbdiscount"
 #kn discount fails when contexts are not events.
-    ngram-count $ngoargs $unkargs $smoothargs -sort -read $count -nonevents $Ev -lm $out $*
-    rm $Ev
+    ngram-count $ngoargs $unkargs $smoothargs $noprune -sort -read $count -nonevents $Ev -lm $out $*
+#    rm $Ev
 }
 
-for f in x.left x.right; do
+for f in $ox.left $ox.right; do
     sort $f | uniq | filt > $f.u
-    filt < $f > $f.d
-    ulm=$f.clm.$N.srilm
+#    filt < $f > $f.d
+    ulm=$f.$N.srilm
     clm_from_counts $f.u $ulm
-    show $f.u $f.d $ulm
+    show $f.u $ulm
 done
+
+wc -l $ruleout $ox*.u
