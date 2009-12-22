a4621b65814e52094aa9714a51cf096d4ea30bb4
Jonathan Graehl <graehl@gmail.com>
2009-12-22 02:26:08 +0000

    correct sri ngram header after stripEF
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2698 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- clm/stripEF.pl
+++ clm/stripEF.pl
@@ -2,7 +2,7 @@
 # input: sorted (ngram_count -sort) srilm cross language ngram
 #  that has two types of tokens: foreign which end in F suffix, english ending in E suffix.
 #  (except for SRI-introduced <s>,</s>,<unk>)
-# output: srilm with fewer ngram events than original (header isn't updated so you should re-count if that matters)
+# output: srilm with fewer ngram events than original (header is updated for new number of N-grams as required by quantize.py and pagh/biglm training)
 # probabilities are always p(f|e-context), so I expect duplicate entries in \N-gram: section
 # -99 ctxE wordE -1.3
 # -2.4 ctxE wordF
@@ -12,38 +12,58 @@
 # all tokens have the E and F suffixes stripped.  (ctx may be 0 or more words)
 
 use warnings;
+use File::Temp;
+my ($fh, $tmpname) = File::Temp::tempfile('stripEF.srilm.XXXXXX');
+print STDERR "using temporary $tmpname\n";
 
 my ($bo,$boline,$N1)=("","",0);
+my $N=0;
+my @ngrams=(0);
+
 sub print1 {
-    ++$N1;
-    print @_;
+    ++$ngrams[$N];
+    print $fh @_;
 }
+
 while(<>) {
-    print;
-    if (/^\\1-grams:$/o) {
-        while(<>) {
-            if (/(\S+)\s+(.*?)(\S+)(F|E(\s+[-\d.e]+)?)\s*$/o) {
-                my ($p,$ctx,$w,$fe,$ebo)=($1,$2,$3,$4,$5);
-                $ctx =~ s/(\S+)[EF](\s+)/$1$2/go;
-                $ev="$ctx$w";
-                if ($p==-99) {
-                    die "F word $w w/ 0 prob: $_" if $fe eq "F";
-                    $bo=defined $ebo ? $ebo : "";
-                    $boline="$p\t$ev$bo\n";
-                    $lastev=$ev;
-                } else {
-                    if ($boline && $lastev ne $ev) {
-                        print($boline);
-                        $bo="";
-                    }
-                    print("$p\t$ev$bo\n");
-                    $boline=$bo="";
-                }
-            } else {
-                die "unknown ngram line: $_" unless (/^(.*<(\/s|s|unk)>)?$/ || /^\\(\d+-grams:)|end\\$/);
-                print($_);
+    if (/^\\(\d+)-grams:\s*$/o) {
+        $N=$1;
+        push @ngrams,0;
+        print STDERR "starting $N-grams...\n";
+    } elsif ($N==0) {
+    } elsif (/(\S+)\s+(.*?)(\S+)(F|E(\s+[-\d.e]+)?)\s*$/o) {
+        my ($p,$ctx,$w,$fe,$ebo)=($1,$2,$3,$4,$5);
+        $ctx =~ s/(\S+)[EF](\s+)/$1$2/go;
+        $ev="$ctx$w";
+        if ($p==-99) {
+            die "F word $w w/ 0 prob: $_" if $fe eq "F";
+            $bo=defined $ebo ? $ebo : "";
+            $boline="$p\t$ev$bo\n";
+            $lastev=$ev;
+        } else {
+            if ($boline && $lastev ne $ev) {
+                print1($boline);
+                $bo="";
             }
+            print1("$p\t$ev$bo\n");
+            $boline=$bo="";
         }
-        last;
+    } elsif (/^(.*<(\/s|s|unk)>)?$/o) {
+        print1($_);
+    } else {
+        die "unknown ngram line: $_" unless /^$/o || /^\\(\d+-grams:|end)\\$/o;
+        print $fh $_;
     }
 }
+
+print "\n\\data\\\n";
+for (1..$#ngrams) {
+    print "ngram $_=$ngrams[$_]\n";
+}
+print "\n";
+
+close $fh;
+
+system "cat",$tmpname;
+
+unlink $tmpname;
