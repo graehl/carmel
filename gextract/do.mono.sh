36e40af8fdf59a62acad5f247ed5c9ee16d8e02a
Jonathan Graehl <graehl@gmail.com>
2010-05-20 20:18:17 +0000

    subset
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2860 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ gextract/do.mono.sh
@@ -0,0 +1,31 @@
+. ~graehl/isd/hints/aliases.sh
+. ~graehl/isd/hints/bashlib.sh
+wd=${wd:-exp}
+noise=${noise:-.1}
+iter=${iter:-50}
+in=${in:-training}
+inlimit=${inlimit:-30}
+nviz=${nviz:-6}
+showvars_required wd noise iter in nviz
+
+mkdir -p $wd
+inm=mono0$noise
+set -e
+sub=$wd/$inm
+./subset-training.py -u $inlimit --pcorrupt=$noise --monotone --inbase=$in --outbase=$sub
+alignbase=$sub.i=$iter
+for s in e-parse f a info; do
+    cp $sub.$s $alignbase.$s
+done
+[ "$noise" == 0 ] && cp $sub.a $sub.a-gold
+#if [ "$iter" -gt 0 ] ; then
+ out=$alignbase.out
+ log=$alignbase.log
+ [ "$skip" ] || ./gextract.py --notest --golda $sub.a-gold --alignment-out $alignbase.a --inbase=$sub --iter=$iter 2>&1 2>&1 >$out | tee $log
+pr=" `lastpr $log`"
+#fi
+vizin=$alignbase.first$nviz
+./subset-training.py --comment="noise=$noise iter=$iter$pr" -l 10 -u 25 -n $nviz --inbase=$alignbase --outbase=$vizin
+lang=eng vizalign $vizin
+showvars pr out log
+grep zeroprob $log || echo "no zeroprob"
