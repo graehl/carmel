88d33aed548fa361cea0c1ecb9199850dec2e1ae
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2010-09-08 05:12:31 +0000

    prange
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@2992 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/io.hpp
+++ shared/io.hpp
@@ -298,6 +298,7 @@ struct getline_reader
 
 
 
+
 struct DefaultWriter
 {
   template <class Ch, class Tr,class value_type>
@@ -315,6 +316,100 @@ struct LineWriter
       return o << l << std::endl;
          }
 };
+
+template <class O, class T,class Writer> inline
+std::ios_base::iostate range_print_iostate(O& o,T begin, T end,Writer writer,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')')
+{
+  static const char *const MULTILINE_SEP="\n";
+  const char space=' ';
+  if (parens) {
+    o << open_paren;
+    if (multiline)
+      o << MULTILINE_SEP;
+  }
+  if (multiline) {
+    for (;begin!=end;++begin) {
+      o << space;
+      deref(writer)(o,*begin);
+      o << MULTILINE_SEP;
+    }
+  } else {
+    word_spacer_c<space> sep;
+    for (;begin!=end;++begin) {
+      o << sep;
+      deref(writer)(o,*begin);
+    }
+  }
+  if (parens) {
+    o << close_paren;
+    if (multiline)
+      o << MULTILINE_SEP;
+  }
+  return GENIOGOOD;
+}
+
+template <class Ib,class Ie,class W=DefaultWriter>
+struct range_formatter {
+  Ib i;
+  Ie e;
+  W w;
+  bool multiline;
+  bool parens;
+  range_formatter(Ib i,Ie e,W w=W(),bool multiline=false,bool parens=true) :
+    i(i),e(e),w(w),multiline(multiline),parens(parens) {}
+
+  template <class Ch, class Tr>
+  std::basic_ostream<Ch,Tr> &
+  operator()(std::basic_ostream<Ch,Tr> &o) const {
+    write_range_iostate(o,i,e,w,multiline,parens);
+    return o;
+  }
+
+  template <class Ch, class Tr>
+  friend inline
+  std::basic_ostream<Ch,Tr> &
+  operator<<(std::basic_ostream<Ch,Tr> &o,range_formatter<Ib,Ie,W> const& w) {
+    return w(o);
+  }
+};
+
+template <class Ib,class Ie,class W>
+range_formatter<Ib,Ie,W>
+wrange(Ib i,Ie e,W const& w,bool multiline=false,bool parens=true)
+{
+  return range_formatter<Ib,Ie,W>(i,e,w,multiline,parens);
+}
+
+template <class Ib,class Ie>
+range_formatter<Ib,Ie>
+prange(Ib i,Ie e,bool multiline=false,bool parens=true)
+{
+  return range_formatter<Ib,Ie,DefaultWriter>(i,e,DefaultWriter(),multiline,parens);
+}
+
+template <class Ch, class Tr, class T,class W> inline
+std::basic_ostream<Ch,Tr> & write_range(std::basic_ostream<Ch,Tr>& o,T begin, T end,W writer,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')')
+{
+  range_print_iostate(o,begin,end,writer,multiline,parens,open_paren,close_paren);
+  return o;
+}
+
+template <class O, class T>
+inline std::ios_base::iostate print_range(O& o,T begin,T end,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')') {
+  return range_print_iostate(o,begin,end,DefaultWriter(),multiline,parens,open_paren,close_paren);
+}
+
+template <class O, class C>
+inline std::ios_base::iostate print_range_i(O& o,C const&c,unsigned from,unsigned to,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')') {
+  return  range_print_iostate(o,c.begin()+from,c.begin()+to,DefaultWriter(),multiline,parens,open_paren,close_paren);
+}
+
+template <class Ch, class Tr, class T,class Writer> inline
+std::basic_ostream<Ch,Tr> & range_print(std::basic_ostream<Ch,Tr>& o,T begin, T end,Writer writer,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')')
+{
+    range_print_iostate(o,begin,end,writer,multiline,parens,open_paren,close_paren);
+    return o;
+}
 
 template <class F,class R>
 struct ReaderCallback : public R
@@ -392,43 +487,6 @@ struct IndirectReader
     }
 };
 
-  template <class O, class T,class Writer> inline
-  std::ios_base::iostate range_print_iostate(O& o,T begin, T end,Writer writer,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')')
-  {
-      static const char *const MULTILINE_SEP="\n";
-      const char space=' ';
-      if (parens) {
-          o << open_paren;
-          if (multiline)
-              o << MULTILINE_SEP;
-      }
-      if (multiline) {
-          for (;begin!=end;++begin) {
-              o << space;
-              deref(writer)(o,*begin);
-              o << MULTILINE_SEP;
-          }
-      } else {
-          word_spacer_c<space> sep;
-          for (;begin!=end;++begin) {
-              o << sep;
-              deref(writer)(o,*begin);
-          }
-      }
-      if (parens) {
-          o << close_paren;
-          if (multiline)
-              o << MULTILINE_SEP;
-      }
-      return GENIOGOOD;
-}
-
-template <class Ch, class Tr, class T,class Writer> inline
-std::basic_ostream<Ch,Tr> & range_print(std::basic_ostream<Ch,Tr>& o,T begin, T end,Writer writer,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')')
-{
-    range_print_iostate(o,begin,end,writer,multiline,parens,open_paren,close_paren);
-    return o;
-}
 
 template <class Dict>
 struct dict_writer
@@ -488,18 +546,6 @@ struct container_writer
         return range_print(o,l.begin(),l.end(),writer,multiline,parens,open_paren,close_paren);
     }
 };
-
-
-
-template <class O, class T>
-inline  std::ios_base::iostate print_range(O& o,T begin, T end,bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')') {
-    return  range_print_iostate(o,begin,end,DefaultWriter(),multiline,parens,open_paren,close_paren);
-}
-
-template <class O, class C>
-inline  std::ios_base::iostate print_range_i(O& o,C const&c,unsigned from, unsigned to, bool multiline=false,bool parens=true,char open_paren='(',char close_paren=')') {
-    return  range_print_iostate(o,c.begin()+from,c.begin()+to,DefaultWriter(),multiline,parens,open_paren,close_paren);
-}
 
   // modifies out iterator.  if returns GENIOBAD then elements might be left partially extracted.  (clear them yourself if you want)
 template <class Ch, class Tr, class Reader, class T>
