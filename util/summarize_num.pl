f21950c531f4aaa49fc3f47fb96b17df962fd58f
Jonathan Graehl <graehl@gmail.com>
2011-01-06 03:04:49 +0000

    summarize
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3082 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

---
+++ util/summarize_num.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+### script info ##################################################
+use File::Basename;
+my $scriptdir; # location of script
+my $scriptname; # filename of script
+my $BLOBS;
+
+BEGIN {
+   $scriptdir = &File::Basename::dirname($0);
+   ($scriptname) = &File::Basename::fileparse($0);
+   push @INC, $scriptdir; # now you can say: require "blah.pl" from the current directory
+    $ENV{BLOBS}='/home/hpc-22/dmarcu/nlg/blobs' unless exists $ENV{BLOBS};
+   $ENV{BLOBS}="$ENV{HOME}/blobs" unless -d $ENV{BLOBS};
+    $BLOBS=$ENV{BLOBS};
+    my $libgraehl="$BLOBS/libgraehl/latest";
+    push @INC,$libgraehl if -d $libgraehl;
+}
+
+require "libgraehl.pl";
+
+### arguments ####################################################
+
+my $template_string='{sOmEnUm}';
+my $raw_out;
+my $sum_out=\*STDOUT;
+my $sums;
+my $grep;
+my $N;
+
+my @options=(
+"Any lines containing integer or floating point numbers are normalized and averages over each unique line (after normalization) are reported",
+             ["template-string=s"=>\$template_string,"Unique string that won't occur anywhere in the input"],
+             ["raw-numbers-out=s"=>\$raw_out,"Write any recognized numbers to this file"],
+             ["sums!"=>\$sums,"Compute sums"],
+             ["grep=s"=>\$grep,"Regexp filtering input lines"],
+             ["n=i"=>\$N,"Only record first n (matching) lines"],
+            );
+
+
+my $cmdline=&escaped_cmdline;
+my ($usagep,@opts)=getoptions_usage(@options);
+info("COMMAND LINE:");
+info($cmdline);
+show_opts(@opts);
+
+
+### main program ################################################
+
+&argvz;
+
+my $loose_num_match=qr/((?:[+\-]|\b)[0123456789]+(?:[.][0123456789]*(?:[eE][0123456789\-+]*)?)?)\b/;
+
+my $RAW=openz_out($raw_out) if $raw_out;
+
+my $l=0;
+while(<>) {
+    next if ($grep && ! /$grep/o);
+    ++$l;
+    if ($N and $l>$N) {
+#        while(<>) {}
+        last;
+    } else {
+        if ($raw_out) {
+            while (/$loose_num_match/g) {
+                print $RAW $1,"\t" if $raw_out;
+            }
+            print $RAW "\n";
+        }
+        log_numbers($_);
+    }
+}
+
+
+&print_number_summary($sum_out,$sums,1);
