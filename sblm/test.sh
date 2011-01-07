b2d19109259b286f41bf8bbda3aa052f0f6d4077
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-01-07 07:55:35 +0000

    pcfg ready to smooth+norm
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3099 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- sblm/test.sh
+++ sblm/test.sh
@@ -1,1 +1,10 @@
-savemap=tmp local=1 iomr-hadoop ${1:-10.eng-parse} ${2:--} ./pcfg-map ./count.py
+. ~graehl/isd/hints/bashlib.sh
+export PATH=~graehl/t/graehl/util:$PATH
+in=${1:-10.eng-parse}
+pre=${pre:-{$in%.eng-parse}.}
+showvars_required in pre
+export local=1
+savemap=tmp.count.map iomr-hadoop $in ${pre}counted ./pcfg-map ./count.py
+savemap=tmp.sums.map iomr-hadoop ${pre}counted ${pre}lhs-sums ./lhs-sums-map ./count.py
+./lhs-sums-map ${pre}counted | mapsort | ./count.py > ${pre}lhs-sums
+./cat-pcfg-for-divide ${pre}lhs-sums ${pre}counted
