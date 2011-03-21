615d90c50a77c802e2ed416944e282617f1158b4
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-03-21 21:41:52 +0000

    graph
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3190 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- dagtt/graph.py
+++ dagtt/graph.py
@@ -22,6 +22,11 @@ class Edge(object):
         self.tail=tail
         self.head=head
         self.color=None
+    def other_v(self,v):
+        "return (u,cost) such that v->u or u->v (reversed cost)."
+        if self.tail==v:
+            return (self.head,self.cost)
+        return (self.tail,self.cost_reverse)
     def reverse(self):
         t=self.tail
         self.tail=self.head
@@ -58,6 +63,27 @@ class Vertices(dict):
     def __missing__(self,key):
         r=self[key]=Vertex(label=key)
         return r
+    def register(self,v):
+        self[v.label]=v
+
+def dfs(v,adj,visit_pre=None,visit_post=None,yield_pre=False,yield_post=False):
+    """pre: all reachable v color were false before recursing with dfs. post: yields visited nodes in """
+    if v.color: return
+    yield=yield_pre or yield_post
+    if yield_pre: yield v
+    if visit_pre: visit(v)
+    v.color=1
+    for e in adj[v]:
+        u,c=e.other_v(v)
+        if u.color: continue
+        if yield:
+            for v in dfs(u,adj,visit_pre,visit_post,yield_pre,yield_post):
+                yield v
+        else:
+            dfs(u,adj,visit_pre,visit_post,yield_pre,yield_post):
+    v.color=2
+    if yield_post: yield v
+    if visit_post: visit(v)
 
 class Graph(object):
     """
@@ -68,33 +94,57 @@ class Graph(object):
     optional label->edge and label->node. note: if you modify labels, heads, or tails, the indices will be out of date
 
     """
+    def clear(self,attr,val=None,e=True,v=True):
+        if e:
+            for e in self.e:
+                setattr(e,attr,val)
+        if v:
+            for v in self.v:
+                setattr(v,attr,val)
     def __init__(self,default_cost_reverse=1):
         self.e=[]
         self.v=Vertices()
         self.default_cost_reverse=default_cost_reverse
+    def vertices(self):
+        return self.v.values()
     def edge(self,tail,head,label=None,cost=0,cost_reverse=None):
         e=Edge(self.v[tail],self.v[head],label,len(self.e),cost,no_none(cost_reverse,self.default_cost_reverse))
         self.e.append(e)
         return e
+    def index_vertices(self):
+        """needed if you didn't add using self.v or self.edge()"""
+        for e in self.e:
+            self.v.register(e.head)
+            self.v.register(e.tail)
     def __str__(self):
         return '[graph v=%s e=%s]'%(self.v.keys(),map(str,self.e))
     def write_edges(self,out):
         write_lines(map(str,self.e),out)
-    def edges_by(self,attr='head'):
+    def edges_by(self,*attrs):
         r=defaultdict(list)
         for e in self.e:
-            r[getattr(e,attr)].append(e)
+            for attr in attrs:
+                r[getattr(e,attr)].append(e)
         return r
-    def index_edges(self):
-        self.byhead=self.edges_by('head')
-        self.bytail=self.edges_by('tail')
+    def adj(self,adjname,*attrs):
+        for e in self.e:
+            for attr in attrs:
+                append_attr(getattr(e,attr),adjname,e)
+    def index_edges(self,head=True,tail=True,undir=True):
+        if head: self.byhead=self.edges_by('head')
+        if tail: self.bytail=self.edges_by('tail')
+        if undir: self.undir=self.edges_by('head','tail')
 
-def graph_main():
+def sample_graph():
     g=Graph()
     g.edge(0,1)
     g.edge(0,2).reverse()
     g.edge(2,1)
     g.edge(1,2)
+    print list(g.dfs(all=True,start=0))
+
+def graph_main():
+    g=sample_graph()
     g.write_edges(sys.stdout)
 
 import optfunc
--- dagtt/invert2dag.py
+++ dagtt/invert2dag.py
@@ -6,13 +6,9 @@
 from graph import *
 
 def invert_main():
-    g=Graph()
-    g.edge(0,1)
-    g.edge(0,2)
-    g.edge(2,1)
-    g.edge(1,2)
-    g.write_edges(sys.stdout)
-    dump(g.edges_by('head'))
+    g=sample_graph()
+    g.index_edges(head=False,tail=False,undir=True)
+    g.clear('color',0,
 
 import optfunc
 optfunc.main(invert_main)
