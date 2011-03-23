f5cfb251ca591b53d76e02fa2191417d95fbb959
Jonathan Graehl <graehl@gmail.com>
2011-03-23 04:11:35 +0000

    dendro
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3193 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- gextract/dendro.py
+++ gextract/dendro.py
@@ -3,14 +3,14 @@ from graehl import *
 def nIDs(n=2):
     return tuple(IDs() for i in range(0,n))
 
-def read_sparse_matrix(m,names,file='up.cluster.eng'):
+def read_sparse_matrix(m,names,file='up.cluster.eng',val=float):
     "last field is value, prev fields are tuple key"
     if type(file)==str:
         file=open(file)
     for line in file:
         fs=line.split()
         key=tuple(names[i][fs[i]] for i in range(0,len(fs)-1))
-        m[key]=fs[-1]
+        m[key]=val(fs[-1])
     return m
 
 def load_sparse_matrix(file):
@@ -18,8 +18,8 @@ def load_sparse_matrix(file):
     return read_sparse_matrix(m,file)
 
 def sparse_to_dense(m,names):
-    nc,nr=map(len,names)
-    ret=[[None]*nc for _ in range(0,nr)]
+    nr,nc=map(len,names)
+    ret=[[0]*nc for _ in range(0,nr)]
     for k,v in m.iteritems():
         r,c=k
         ret[r][c]=v
@@ -45,7 +45,7 @@ from matplotlib.pyplot import show
 from hcluster import pdist, linkage, dendrogram
 import numpy
 from scipy import sparse
-def dendro(X,metric='cosine',combine='average',showdendro=True,leaf_label_func=id2str,**kw):
+def dendro(X,metric='cosine',combine='average',showdendro=True,leaf_label_func=identity,**kw):
     Y = pdist(X,metric)
     Z = linkage(Y,combine)
     if showdendro:
@@ -53,12 +53,12 @@ def dendro(X,metric='cosine',combine='average',showdendro=True,leaf_label_func=i
         show()
     return Z
 
-def test(m=None,n=50000):
+def test(file='up.cluster',m=None,n=50000):
     if m is None:
         m=dict()
-    n=nIDs()
-    read_sparse_matrix(m,n)
-    print_sparse_matrix(m,n)
-    d=sparse_to_dense(m,n)
-    dendro(d)
+    names=nIDs()
+    read_sparse_matrix(m,names,file=file)
+    #print_sparse_matrix(m,names)
+    d=sparse_to_dense(m,names)
+    dendro(d,leaf_label_func=lambda x:names[0].token(x),leaf_font_size=10)
 
