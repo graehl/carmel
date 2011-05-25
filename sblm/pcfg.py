cf8dea93f69e4f88a189d0447f762ef656efef38
Jonathan Graehl <graehl@gmail.com>
2011-05-25 19:07:36 +0000

    from hpc
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3274 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- sblm/pcfg.py
+++ sblm/pcfg.py
@@ -672,15 +672,16 @@ class sblm_ngram(object):
                 else:
                     log("PCFG (%s): sum=1:%d/(N=%d)=%s sum<1:%d/N=%s sum>1:%d/N=%s sum>1"%(p,n1,total,n1/total,nlt1,nlt1/total,len(gt1),len(gt1)/total))
         log("PCFG sum=1: "+' '.join(sum1))
-    def train_lm(self,prefix=None,lmf=None,uni_witten_bell=True,uni_unkword=None,ngram_witten_bell=False,sri_ngram_count=False,check=True,write_lm=False,merge_terminals=True,sort=True):
+    def train_lm(self,prefix=None,lmf=None,uni_witten_bell=True,uni_unkword=None,ngram_witten_bell=False,sri_ngram_count=False,check=True,write_lm=False,merge_terminals=True,sort=True,terminals=True):
         prefixterm=None
         if prefix is not None:
             prefixterm=prefix+'.terminals'
             prefix+='.pcfg'
         mkdir_parent(prefix)
-        self.terminals.train(uni_witten_bell,uni_unkword)
+        if (terminals):
+            self.terminals.train(uni_witten_bell,uni_unkword)
         all=self.ng.train_lm(prefix=prefix,sort=sort,lmf=lmf,witten_bell=ngram_witten_bell,read_lm=True,sri_ngram_count=sri_ngram_count,write_lm=(write_lm and not merge_terminals))
-        if merge_terminals:
+        if terminals and merge_terminals:
             self.ng.disjoint_add_lm(self.terminals.ngram_lm(),conflict_take_bow_only=True) #NOTE: for preterms, we want the bow from the terminal model, and the backed off unigram prob from the PCFG rewrite model
             # without doing this we have from pcfg sblm (in merged result): -2.51869334212  VBP-0   -2.59911856506
             # but from terminal 2gram: -99     VBP-0   -2.73926926467
@@ -692,9 +693,9 @@ class sblm_ngram(object):
             # -0.0103815795832        @VBP-0-BAR VBP-0 VBP-0  -3.11260500153
             # VBP-0(@VBP-0-BAR VBP-0)
             # what we could do if we wanted to make counts of as-NT vs as-PT compete: keep unigram backoff events for lex vs nonlex sep, but otherwise let bigram (given parent) compete? also need to choose-one-child <s> "lex" </s>?
-            if write_lm:
-                self.ng.write_lm(lmf,prefix=prefix,sort=sort)
-        if write_lm and prefixterm is not None:
+        if write_lm:
+            self.ng.write_lm(lmf,prefix=prefix,sort=sort)
+        if terminals and write_lm and prefixterm is not None:
             self.terminals.write_lm(prefixterm)
         if self.parent:
             for nt,png in self.png.iteritems():
@@ -706,43 +707,46 @@ class sblm_ngram(object):
                 pntf=png.train_lm(prefix=pnt,sort=True,lmf=plmf,witten_bell=ngram_witten_bell,read_lm=True,sri_ngram_count=sri_ngram_count,write_lm=write_lm)
                 #dump('lm for',nt,plmf)
     def __str__(self):
-        return str(self.terminals)
+        return "PCFG[...]"
 
 dev='sample/dev.e-parse'
 test='sample/test.e-parse'
 train='sample/training.e-parse'
 #train='training.e-parse'
-
+@optfunc.arghelp('t_train','input radu parses one per line')
+@optfunc.arghelp('terminals','include PRETERM("terminal") events')
 def pcfg_ngram_main(n=5,
-                    train=train
+                    t_train=train
                     ,eval=False
                     ,dev=dev
                     ,test=test
-                    ,parent=True
+                    ,p_parent=True
                     ,parent_alpha=0.999
-                    ,cond_parent=True
-                    ,witten_bell=True
+                    ,c_cond_parent=True
+                    ,w_witten_bell=True
 #                    ,logfile="ppx.dev.txt"
                     ,eval_logfile="test.sri.txt"
                     ,sri_ngram_count=False
                     ,digit2at=True
                     ,write_lm=True
                     ,merge_terminals=True
+                    ,terminals=True
                     ,skip_bar=True
                     ,unsplit=True
-                    ,outpre=""
+                    ,o_outpre=""
                     ,logp_unk=0.0
                     #,logp_glue=0.0 #what about GLUE0 etc?
                     ,bo_witten=0.1
                     ):
     log('pcfg_ngram')
     log(str(Locals()))
-    sb=sblm_ngram(order=n,parent=parent,parent_alpha=parent_alpha,cond_parent=cond_parent,skip_bar=skip_bar,unsplit=unsplit,digit2at=digit2at,witten_bo=bo_witten,logp_unk=logp_unk)
+    sb=sblm_ngram(order=n,parent=p_parent,parent_alpha=parent_alpha,cond_parent=c_cond_parent,skip_bar=skip_bar,unsplit=unsplit,digit2at=digit2at,witten_bo=bo_witten,logp_unk=logp_unk)
+    outpre=o_outpre
     if len(outpre)==0: outpre=train
     #dumpx(str(sb.tree_from_line('(S-2 (@S-BAR (A "a"  ) (B-2 "b"  ) ) )')))
-    ntrain=sb.read_radu(train)
+    ntrain=sb.read_radu(t_train)
     s=Stopwatch('Train')
-    sb.train_lm(prefix=outpre+'.sblm/sblm',sri_ngram_count=sri_ngram_count,ngram_witten_bell=witten_bell,write_lm=write_lm,merge_terminals=merge_terminals)
+    sb.train_lm(prefix=o_outpre+'.sblm/sblm',sri_ngram_count=sri_ngram_count,ngram_witten_bell=w_witten_bell,write_lm=write_lm,merge_terminals=merge_terminals,terminals=terminals)
     log('# training nodes: %s'%ntrain)
     log('lm file: %s'%sb.ng.lmfile())
     warn(s)
