5ab66f7bae09ac4c21315d04d39ea5d3b64427cc
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-06-16 00:36:07 +0000

    small_vector append_ra fix, added more tests
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3342 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/small_vector.hpp
+++ shared/small_vector.hpp
@@ -98,11 +98,12 @@ class small_vector {
     } else
       for (size_type i = 0; i < s; ++i,++begin) data_.ptr[i] = *begin;
   }
+
   small_vector(T const* i,T const* end) {
     int s=end-i;
     assert(s<=size_max);
     Alloc(s);
-    std::memcpy(this->begin(),i,s*sizeof(T));
+    memcpy(i);
   }
 
   small_vector(const Self& o) : size_(o.size_) {
@@ -120,7 +121,7 @@ class small_vector {
   T *erase(T *b) {
     return erase(b,b+1);
   }
-  T *erase(T *b,T* e) {
+  T *erase(T *b,T* e) { // remove [b,e) and return pointer to element e
     T *tb=begin(),*te=end();
     int nbefore=b-tb;
     if (e==te) {
@@ -132,10 +133,17 @@ class small_vector {
     }
     return begin()+nbefore;
   }
-  void memcpy_heap(T *p) {
+  void memcpy_to(T *p) {
+    std::memcpy(p,begin(),size_*sizeof(T));
+  }
+  void memcpy_heap(T const* p) {
     assert(size_<=size_max);
     assert(capacity_>=size_);
     std::memcpy(data_.ptr,p,size_*sizeof(T));
+  }
+  void memcpy(T const* p) {
+    assert(size_<=size_max);
+    std::memcpy(this->begin(),p,size_*sizeof(T));
   }
   const Self& operator=(const Self& o) {
     if (size_ <= SV_MAX) {
@@ -159,7 +167,7 @@ class small_vector {
           Alloc_heap();
         }
         size_ = o.size_;
-        memcpy_heap(o.data_ptr);
+        memcpy_heap(o.data_.ptr);
         for (int i = 0; i < size_; ++i)
           data_.ptr[i] = o.data_.ptr[i];
       }
@@ -183,31 +191,57 @@ class small_vector {
   bool empty() const { return size_ == 0; }
   size_t size() const { return size_; }
 
-  inline void reserve(size_type sz) {
-    if (size_<=SV_MAX) return;
-    ensure_capacity(sz);
+  inline void resize_up(size_type s) { // like reserve, but because of capacity_ undef if size_<=SV_MAX, must set size_ immediately or we lose invariant
+    assert(s>size_);
+    if (s>SV_MAX)
+      reserve_up_big(s);
+    size_=s;
+  }
+  void resize_up_big(size_type s) {
+    reserve_up_big(s);
+    size_=s;
+  }
+  void reserve(size_type s) {
+    if (size_>SV_MAX)
+      reserve_up_big(s);
   }
 
-  inline void ensure_capacity(size_type min_size) {
+private:
+  void reserve_up_big(size_type s) {
+    assert(s>SV_MAX);
+    if (size_>SV_MAX) {
+      if (s>capacity_)
+        ensure_capacity(s);
+    } else {
+      capacity_=s;
+      T* tmp = Alloc_heap(s);
+      std::memcpy(tmp, data_.vals, SV_MAX*sizeof(T)); // const instead of size_
+      data_.ptr=tmp;
+    }
+  }
+  void ensure_capacity(size_type min_size) {
+    // only call if you're already large
     assert(min_size > SV_MAX);
+    assert(size_ > SV_MAX);
     if (min_size < capacity_) return;
     size_type new_cap = std::max(static_cast<size_type>(capacity_ << 1), min_size);
     T* tmp = Alloc_heap(new_cap);
-    std::memcpy(tmp, data_.ptr, capacity_ * sizeof(T));
+    std::memcpy(tmp, data_.ptr, size_ * sizeof(T));
     Free_heap();
     data_.ptr = tmp;
     capacity_ = new_cap;
   }
 
-private:
-  inline void copy_vals_to_ptr() {
-    capacity_ = SV_MAX * 2;
-    T* tmp = new T[capacity_];
-    for (int i = 0; i < SV_MAX; ++i) tmp[i] = data_.vals[i];
-    data_.ptr = tmp;
-  }
-  inline void ptr_to_small() {
+  void copy_vals_to_ptr() {
     assert(size_<=SV_MAX);
+    capacity_ = SV_MAX * 2;
+    T* tmp = Alloc_heap(capacity_);
+    std::memcpy(tmp,data_.vals,SV_MAX*sizeof(T)); // may copy more than actual size_. ok. constant should be more optimizable
+    data_.ptr = tmp;
+    // only call if you're going to immediately increase size_ to >SV_MAX
+  }
+  void ptr_to_small() {
+    assert(size_<=SV_MAX); // you decreased size_ already. normally ptr wouldn't be used if size_ were this small
     T *tmp=data_.ptr; // should be no problem with memory access order (strict aliasing) because of safe access through union
     for (size_type i=0;i<size_;++i) // no need to memcpy for small size
       data_.vals[i]=tmp[i];
@@ -216,6 +250,7 @@ private:
 
 public:
 
+  // aliasing warning: if append is within self, undefined result (because we invalidate iterators as we grow)
   template <class I>
   inline void append(I i,I end) {
     for (;i!=end;++i)
@@ -223,22 +258,35 @@ public:
   }
 
   template <class I>
-  inline void append_ra(I i,I end) {
-    reserve(size_+(end-i));
+  inline void append_ra(I i,I e) {
+    size_type s=size_;
+    resize_up(size_+(e-i));
+    assert(size_==s+e-i);
+    for (T *b=begin()+s;i<e;++i,++b) {
+      assert(b<end());
+      *b=*i;
+    }
+  }
+  inline void append_ra(T const* i,T const* end) {
+    size_type s=size_;
+    size_type n=end-i;
+    resize_up(size_+n);
+    std::memcpy(begin()+s,i,n*sizeof(T));
   }
 
   inline void push_back(T const& v) {
     if (size_ < SV_MAX) {
       data_.vals[size_] = v;
       ++size_;
-      return;
     } else if (size_ == SV_MAX) {
       copy_vals_to_ptr();
-    } else if (size_ == capacity_) {
-      ensure_capacity(size_ + 1);
+      data_.ptr[SV_MAX]=v;
+      size_=SV_MAX+1;
+    } else {
+      if (size_ == capacity_)
+        ensure_capacity(size_+1);
+      data_.ptr[size_++] = v;
     }
-    data_.ptr[size_] = v;
-    ++size_;
   }
 
   T& back() { return this->operator[](size_ - 1); }
@@ -273,32 +321,22 @@ public:
     assert(size_<=size_max);
     if (s <= SV_MAX) {
       if (size_ > SV_MAX) {
-        T *tmp=data_.ptr;
-        for (size_type i = 0; i < s; ++i) data_.vals[i] = tmp[i];
-        delete[] tmp;
-        size_ = s;
+        size_=s;
+        ptr_to_small();
         return;
-      }
-      if (s <= size_) {
-        size_ = s;
-        return;
-      } else {
+      } else if (s<=size_) {
+      } else { // growing but still small
         for (size_type i = size_; i < s; ++i)
           data_.vals[i] = v;
-        size_ = s;
-        return;
       }
-    } else {
-      if (size_ <= SV_MAX)
-        copy_vals_to_ptr();
-      if (s > capacity_)
-        ensure_capacity(s);
+    } else { // new s is large
       if (s > size_) {
-        for (size_type i = size_; i < s; ++i)
+        reserve_up_big(s);
+        for (size_type i = size_;i<s;++i)
           data_.ptr[i] = v;
       }
-      size_ = s;
     }
+    size_=s;
   }
 
   T& operator[](size_t i) {
@@ -357,12 +395,12 @@ public:
 
  private:
   union StorageType { // should be safe from strict-aliasing optimizations
-    T vals[SV_MAX];
-    T* ptr;
+    T vals[SV_MAX]; // iff size_<=SV_MAX (note: tricky! anything that shrinks the array may mean copying back to vals)
+    T* ptr; // otherwise
   };
   StorageType data_;
   size_type size_;
-  size_type capacity_;  // only defined when size_ > SV_MAX
+  size_type capacity_;  // only initialized when size_ > SV_MAX
 };
 
 template <class T,int M>
@@ -382,7 +420,7 @@ void memcpy(void *out,small_vector<T,M> const& v) {
 
 }//ns
 
-#if defined(TEST) && 0
+#if defined(TEST)
 #include <graehl/shared/test.hpp>
 
 typedef graehl::small_vector<int,2> SmallVectorInt;
@@ -393,73 +431,80 @@ BOOST_AUTO_TEST_CASE( test_small_vector_larger_than_2 ) {
   SmallVectorInt v2;
   v.push_back(0);
   v.push_back(1);
+  EXPECT_EQ(v[1],1);
+  EXPECT_EQ(v[0],0);
   v.push_back(2);
-  assert(v.size() == 3);
-  assert(v[2] == 2);
-  assert(v[1] == 1);
-  assert(v[0] == 0);
+  EXPECT_EQ(v.size(),3);
+  EXPECT_EQ(v[2],2);
+  EXPECT_EQ(v[1],1);
+  EXPECT_EQ(v[0],0);
   v2 = v;
   SmallVectorInt copy(v);
-  assert(copy.size() == 3);
-  assert(copy[0] == 0);
-  assert(copy[1] == 1);
-  assert(copy[2] == 2);
-  assert(copy == v2);
+  EXPECT_EQ(copy.size(),3);
+  EXPECT_EQ(copy[0],0);
+  EXPECT_EQ(copy[1],1);
+  EXPECT_EQ(copy[2],2);
+  EXPECT_EQ(copy,v2);
   copy[1] = 99;
-  assert(copy != v2);
-  assert(v2.size() == 3);
-  assert(v2[2] == 2);
-  assert(v2[1] == 1);
-  assert(v2[0] == 0);
+  EXPECT_TRUE(copy != v2);
+  EXPECT_EQ(v2.size(),3);
+  EXPECT_EQ(v2[2],2);
+  EXPECT_EQ(v2[1],1);
+  EXPECT_EQ(v2[0],0);
   v2[0] = -2;
   v2[1] = -1;
   v2[2] = 0;
-  assert(v2[2] == 0);
-  assert(v2[1] == -1);
-  assert(v2[0] == -2);
+  EXPECT_EQ(v2[2],0);
+  EXPECT_EQ(v2[1],-1);
+  EXPECT_EQ(v2[0],-2);
   SmallVectorInt v3(1,1);
-  assert(v3[0] == 1);
+  EXPECT_EQ(v3[0],1);
   v2 = v3;
-  assert(v2.size() == 1);
-  assert(v2[0] == 1);
+  EXPECT_EQ(v2.size(),1);
+  EXPECT_EQ(v2[0],1);
   SmallVectorInt v4(10, 1);
-  assert(v4.size() == 10);
-  assert(v4[5] == 1);
-  assert(v4[9] == 1);
+  EXPECT_EQ(v4.size(),10);
+  EXPECT_EQ(v4[5],1);
+  EXPECT_EQ(v4[9],1);
   v4 = v;
-  assert(v4.size() == 3);
-  assert(v4[2] == 2);
-  assert(v4[1] == 1);
-  assert(v4[0] == 0);
+  EXPECT_EQ(v4.size(),3);
+  EXPECT_EQ(v4[2],2);
+  EXPECT_EQ(v4[1],1);
+  EXPECT_EQ(v4[0],0);
   SmallVectorInt v5(10, 2);
-  assert(v5.size() == 10);
-  assert(v5[7] == 2);
-  assert(v5[0] == 2);
-  assert(v.size() == 3);
+  EXPECT_EQ(v5.size(),10);
+  EXPECT_EQ(v5[7],2);
+  EXPECT_EQ(v5[0],2);
+  EXPECT_EQ(v.size(),3);
   v = v5;
-  assert(v.size() == 10);
-  assert(v[2] == 2);
-  assert(v[9] == 2);
+  EXPECT_EQ(v.size(),10);
+  EXPECT_EQ(v[2],2);
+  EXPECT_EQ(v[9],2);
   SmallVectorInt cc;
-  for (int i = 0; i < 33; ++i)
+  for (int i = 0; i < 33; ++i) {
     cc.push_back(i);
+    EXPECT_EQ(cc.size(),i+1);
+    EXPECT_EQ(cc[i],i);
+    for (int j=0;j<i;++j)
+      EXPECT_EQ(cc[j],j);
+  }
   for (int i = 0; i < 33; ++i)
-    assert(cc[i] == i);
+    EXPECT_EQ(cc[i],i);
   cc.resize(20);
-  assert(cc.size() == 20);
+  EXPECT_EQ(cc.size(),20);
   for (int i = 0; i < 20; ++i)
-    assert(cc[i] == i);
+    EXPECT_EQ(cc[i],i);
   cc[0]=-1;
   cc.resize(1, 999);
-  assert(cc.size() == 1);
-  assert(cc[0] == -1);
+  EXPECT_EQ(cc.size(),1);
+  EXPECT_EQ(cc[0],-1);
   cc.resize(99, 99);
   for (int i = 1; i < 99; ++i) {
     //cerr << i << " " << cc[i] << endl;
-    assert(cc[i] == 99);
+    EXPECT_EQ(cc[i],99);
   }
   cc.clear();
-  assert(cc.size() == 0);
+  EXPECT_EQ(cc.size(),0);
 }
 
 
@@ -471,7 +516,7 @@ BOOST_AUTO_TEST_CASE( test_small_vector_small )
   SmallVectorInt v2(2,10);
   SmallVectorInt v1a(2,0);
   EXPECT_TRUE(v1 != v1a);
-  EXPECT_TRUE(v1 == v1);
+  EXPECT_EQ(v1,v1);
   EXPECT_EQ(v1[0], 0);
   EXPECT_EQ(v2[1], 10);
   EXPECT_EQ(v2[0], 10);
@@ -480,24 +525,35 @@ BOOST_AUTO_TEST_CASE( test_small_vector_small )
   EXPECT_EQ(v2[0], 9);
   EXPECT_EQ(v2[1], 11);
   SmallVectorInt v3(v2);
-  assert(v3[0] == 9);
-  assert(v3[1] == 11);
+  EXPECT_EQ(v3[0],9);
+  EXPECT_EQ(v3[1],11);
   assert(!v3.empty());
-  assert(v3.size() == 2);
+  EXPECT_EQ(v3.size(),2);
   v3.clear();
   assert(v3.empty());
-  assert(v3.size() == 0);
-  assert(v3 != v2);
-  assert(v2 != v3);
+  EXPECT_EQ(v3.size(),0);
+  EXPECT_TRUE(v3 != v2);
+  EXPECT_TRUE(v2 != v3);
   v3 = v2;
-  assert(v3 == v2);
-  assert(v2 == v3);
-  assert(v3[0] == 9);
-  assert(v3[1] == 11);
+  EXPECT_EQ(v3,v2);
+  EXPECT_EQ(v2,v3);
+  EXPECT_EQ(v3[0],9);
+  EXPECT_EQ(v3[1],11);
   assert(!v3.empty());
-  assert(v3.size() == 2);
-  //cerr << sizeof(SmallVectorInt) << endl;
-  //cerr << sizeof(vector<int>) << endl;
+  EXPECT_EQ(v3.size(),2);
+  SmallVectorInt v4=v3;
+  v3.append_ra(v4.begin()+1,v4.end());
+  EXPECT_EQ(v3.size(),3);
+  EXPECT_EQ(v3[2],11);
+  v4=v3; // avoid aliasing
+  v3.append_ra(v4.begin(),v4.end());
+  v4=v3;
+  EXPECT_EQ(v3.size(),6);
+  v3.append_ra((int const *)v4.begin(),(int const *)v4.end());
+  EXPECT_EQ(v3.size(),12);
+  EXPECT_EQ(v3[11],11);
+//  cerr << "sizeof SmallVectorInt="<<sizeof(SmallVectorInt) << endl;
+//  cerr << "sizeof vector="<<sizeof(vector<int>) << endl;
 }
 #endif
 
