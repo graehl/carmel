fc00a8dd3df8f05b43165e3148a5246d79a6999f
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-07-12 01:15:40 +0000

    pypy
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3429 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- util/datespan.py
+++ util/datespan.py
@@ -3,4 +3,10 @@ from graehl import *
 import sys
 
 for x in sys.argv[1:]:
-    print '%s\n'%filedatespan(x)
+    l,u=filedaterange(x,False)
+    sys.stderr.write('%s %s\n'%(l,u))
+    a=u-l
+    l,u=(min(ctime(x),l),max(mtime(x),u))
+    sys.stderr.write('%s %s\n'%(l,u))
+    b=u-l
+    print '%s\n%s\n\n'%(a,b),
