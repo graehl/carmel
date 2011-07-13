44abb7f692b8a0f633c321376b63272f3d7aa94d
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-07-13 07:16:02 +0000

    sblm-nts
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3431 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- util/datespan.py
+++ util/datespan.py
@@ -5,8 +5,8 @@ import sys
 for x in sys.argv[1:]:
     l,u=filedaterange(x,False)
     sys.stderr.write('%s %s\n'%(l,u))
-    a=u-l
-    l,u=(min(ctime(x),l),max(mtime(x),u))
+    a=u-l if (u is not None and l is not None) else '???'
+    l,u=(min(nonone((ctime(x),l))),max(nonone((mtime(x),u))))
     sys.stderr.write('%s %s\n'%(l,u))
     b=u-l
     print '%s\n%s\n\n'%(a,b),
