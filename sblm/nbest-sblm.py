a755d4362c5bc36360190900492792e54868a303
Jonathan Graehl <graehl@gmail.com>
2011-07-22 00:05:25 +0000

    remove old sb spc from nbest and add newly computed
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3445 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- sblm/nbest-sblm.py
+++ sblm/nbest-sblm.py
@@ -18,17 +18,21 @@ from pcfg import *
 from tree import *
 from nbest import *
 
+sbpre='sb'
+pcpre='spc'
+
 unks=set([])
 def tounk(x):
     return '<unk>' if x.startswith('GLUE') or x in unks else x
 
-def check_nbest(l,lm,term=True,strip=True,flatten=True,num2at=True,output_nbest='',maxnodes=999999):
-    output_nbest=None if output_nbest=='' else open(output_nbest,'w')
+def check_nbest(l,lm,term=True,strip=True,flatten=True,num2at=True,output_nbest=None,maxnodes=999999,lineno='?'):
     trees=getfield_brace('tree',l)
     t=str_to_tree(trees)
     if t is None:
-        warn("no tree for %s line %s"%(trees,l))
-        return
+        warn("no tree for %s line #%s"%(trees,lineno))
+        return False
+    if t.size()>maxnodes:
+        return False
     rt=str(t)
     if trees!=rt:
         s=' '.join(smallest_difference([trees,rt],porch=2))
@@ -37,33 +41,33 @@ def check_nbest(l,lm,term=True,strip=True,flatten=True,num2at=True,output_nbest=
         return tounk(sbmt_lhs_label(x,num2at))
     tm=t.mapnode(label)
     def skiplabel(y):
-        #        dump('skiplabel',y)
         if strip:
             y=strip_subcat(y)
         return no_bar(y) if flatten else y
     tm=tm.map_skipping(skiplabel)
     fv=getfields_num(l)
-    sb=inds(fv,'sb')
-    pc=inds(fv,'spc')
+    sb=inds(fv,sbpre)
+    pc=inds(fv,pcpre)
     fvd=dict(fv)
     sent=fvd['sent']
     sbt=IntDict()
     pct=IntDict()
     def vpc(p,c):
-#        dump(p,c)
         pct[(p,c)]+=1
     def vlr(left,right):
-#        dump(left,right)
         sbt[(left,no_none(right,'</s>'))]+=1
-    dump(sent,str(tm))
     tm.visit_pcl(vpc,leaf=False,root=False)
     tm.visit_lrl(vlr,leaf=False,left='<s>',right='</s>')
     head='sent=%s tree=%s tree-orig=%s'%(sent,tm,trees)
     if len(sb):
-        warn_diff(sb,sbt,desc='sb',header=head)
+        warn_diff(sb,sbt,desc=sbpre,header=head)
     if len(pc):
-        warn_diff(pc,pct,desc='pc',header=head)
-    #    log(str(tm))
+        warn_diff(pc,pct,desc=pcpre,header=head)
+    if output_nbest is not None:
+        s=stripinds(l.rstrip(),'%s|%s'%(sbpre,pcpre))
+        dump(s)
+        output_nbest.write('%s %s %s\n'%(s,strinds(sbpre,sbt),strinds(pcpre,pct)))
+    return True
 
 @optfunc.arghelp('lm','SRI ngram trained by pcfg.py')
 @optfunc.arghelp('nbest','sbmt_decoder nbest list (optionally with sblm score debugging logs before each nbest)')
@@ -82,9 +86,15 @@ def nbest_sblm_main(lm='nbest.pcfg.srilm',
     #outlm='rewrite.'+lm
     #n.write_lm(outlm)
     dump(nbest)
+    output_nbest=None if output_nbest=='' else open(output_nbest,'w')
+    n=0
+    ng=0
     for l in open(nbest):
         if l.startswith("NBEST sent="):
-            check_nbest(l,n,term,strip,flatten,num2at,output_nbest,maxnodes)
+            n+=1
+            if check_nbest(l,n,term,strip,flatten,num2at,output_nbest,maxnodes,lineno=n):
+                ng+=1
+    log("%s good out of %s NBEST lines"%(ng,n))
 
 import optfunc
 optfunc.main(nbest_sblm_main)
