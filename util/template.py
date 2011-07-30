4ddf913b421a7e10734c1ed749c89851475f9ea4
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-07-30 03:15:22 +0000

    diff-features
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3463 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- util/template.py
+++ util/template.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python2.7
+"""
+the purpose of this script.
+"""
+
 from graehl import *
 from collections import defaultdict
+import optfunc
 
+@optfunc.arghelp('rest_','input files')
 def main(rest_=['-'],keyfields=1,sep='\t'):
+    """-h usage"""
     for f in rest_:
         for l in open_in(f):
             print sep.join(l.split(sep)[0:keyfields])
 
-import optfunc
 optfunc.main(main)
