58e52168b0bef9f2d950895a503075ffe7d6ce94
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-11-17 02:35:35 +0000

    fileargs - throw when open fails
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3523 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/fileargs.hpp
+++ shared/fileargs.hpp
@@ -208,14 +208,15 @@ the buffer will be back to 8k.
     template <class filestream>
     void set_new_buf(std::string const& filename,std::string const& fail_msg="Couldn't open file",bool large_buf=true)
     {
-        std::auto_ptr<filestream> f(new filestream());
-        set_checked(*f,filename,delete_after,fail_msg); // exception safety provided by f
-        if (large_buf) give_large_buf();
-        const bool read=stream_traits<filestream>::read;
-        f->open(filename.c_str(),std::ios::binary | (read ? std::ios::in : (std::ios::out|std::ios::trunc)));
-        if (!*f)
-          throw_fail(filename,read?"Couldn't open for input.":"Couldn't open for output.");
-        f.release(); // delete later so we have valid open pointer
+      filestream *f=new filestream();
+      std::auto_ptr<filestream> fa(f);
+      set_checked(*f,filename,delete_after,fail_msg); // exception safety provided by f
+      fa.release(); // now owned by smart ptr
+      if (large_buf) give_large_buf();
+      const bool read=stream_traits<filestream>::read;
+      f->open(filename.c_str(),std::ios::binary | (read ? std::ios::in : (std::ios::out|std::ios::trunc)));
+      if (!*f)
+        throw_fail(filename,read?"Couldn't open for input.":"Couldn't open for output.");
     }
 
     void give_large_buf()
