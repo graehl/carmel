dcab14c761885669a10ec234f998bf3aae662d90
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-12-07 07:18:04 +0000

    fixed atomic_intrusive_refcount
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3549 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/intrusive_refcount.hpp
+++ shared/intrusive_refcount.hpp
@@ -4,8 +4,8 @@
 #include <boost/intrusive_ptr.hpp>
 #include <boost/noncopyable.hpp>
 #include <boost/detail/atomic_count.hpp>
+#include <boost/pool/pool.hpp>
 #include <cassert>
-#include <memory>
 
 /**
 
@@ -21,12 +21,13 @@
 
 */
 
-namespace boost {
+namespace graehl {
 // note: the free functions need to be in boost namespace, OR namespace of involved type. this is the only way to do it.
 
-template<class T,class R=unsigned>
+template<class T,class U=boost::default_user_allocator_new_delete,class R=unsigned>
 struct intrusive_refcount //: boost::noncopyable
 {
+  typedef U user_allocator;
   typedef T pointed_type;
   friend void intrusive_ptr_add_ref(T const* ptr)
   {
@@ -35,37 +36,42 @@ struct intrusive_refcount //: boost::noncopyable
 
   friend void intrusive_ptr_release(T const* ptr)
   {
-    if (--((const intrusive_refcount *)ptr)->refcount)
-      delete ptr;
+    if (--((const intrusive_refcount *)ptr)->refcount) {
+      ptr->~T();
+      U::free((void *)ptr);
+    }
   }
 protected:
-  intrusive_refcount(): refcount(0) {}
+  intrusive_refcount() : refcount(0) {}
   ~intrusive_refcount() { assert(refcount==0); }
 private:
   mutable R refcount;
 };
 
 //template typedef
-template <class T>
+template <class T,class U=boost::default_user_allocator_new_delete>
 struct atomic_refcount
 {
-  typedef intrusive_refcount<T,boost::detail::atomic_count> type;
+  typedef intrusive_refcount<T,U,boost::detail::atomic_count> type;
 };
 
-// on the other hand, this is more usable than a template typedef.
-template<class T>
+// on the other hand, this is less typing to use than the equivalent above typedef
+template<class T,class U=boost::default_user_allocator_new_delete>
 struct atomic_intrusive_refcount //: boost::noncopyable
 {
+  typedef U user_allocator;
   typedef T pointed_type;
-  friend void atomic_intrusive_ptr_add_ref(T const* ptr)
+  friend void intrusive_ptr_add_ref(T const* ptr)
   {
     ++((const atomic_intrusive_refcount *)ptr)->refcount;
   }
 
-  friend void atomic_intrusive_ptr_release(T const* ptr)
+  friend void intrusive_ptr_release(T const* ptr)
   {
-    if (--((const atomic_intrusive_refcount *)ptr)->refcount)
-      delete ptr;
+    if (--((const atomic_intrusive_refcount *)ptr)->refcount) {
+      ptr->~T();
+      U::free((void *)ptr);
+    }
   }
 protected:
   atomic_intrusive_refcount(): refcount(0) {}
@@ -75,44 +81,50 @@ private:
   mutable R refcount;
 };
 
-
-struct delete_any
+template <class T,class U>
+void construct()
 {
-  template <class V>
-  void operator()(V const* v) const
-  {
-    delete v;
-  }
+  T *p=(T*)U::malloc(sizeof(T));
+  new(&p) T();
+  return p;
+}
+
+template <class T,class U,class T1>
+void construct(T1 const& t1)
+{
+  T *p=(T*)U::malloc(sizeof(T));
+  new(&p) T(t1);
+  return p;
+}
+
+template <class T,class U,class T1>
+void construct(T1 & t1)
+{
+  T *p=(T*)U::malloc(sizeof(T));
+  new(&p) T(t1);
+  return p;
+}
+
+template <class T>
+struct intrusive_traits;
+
+template <class T>
+struct intrusive_traits
+{
+  typedef typename T::user_allocator user_allocator;
+  //TODO: thread_safe constant? only true for atomic_count?
 };
 
-template<class T,class D=delete_any,class R=unsigned>
-struct intrusive_refcount_destroy : protected D //: boost::noncopyable
-{
-  typedef T pointed_type;
-  D & destroyer() const { return *((D *)this); }
-  intrusive_refcount_destroy(D const& d) : D(d) {}
-  intrusive_refcount_destroy() {}
-  void set_destroyer(D const& d) { destroyer() = d; }
-// typedef intrusive_refcount_destroy<T> pointed_type;
-  friend void intrusive_ptr_add_ref(T const* ptr)
-  {
-    ++((const intrusive_refcount_destroy *)ptr)->refcount;
-  }
 
-  friend void intrusive_ptr_release(T const* ptr)
-  {
-    if (--((const intrusive_refcount_destroy *)ptr)->refcount)
-      destroyer()(ptr);
-  }
-protected:
-  intrusive_refcount_destroy(): refcount(0) {}
-  ~intrusive_refcount_destroy() { assert(refcount==0); }
-private:
-  mutable R refcount;
-};
+//etc
 
+}//ns graehl
+
+namespace boost {
+
+using graehl::intrusive_refcount;
+using graehl::atomic_intrusive_refcount; // for backward compat - I used to think these had to be declared in boost ns
 
 }//ns
-
 
 #endif
