69bfa6172ae46f51ca7e6e73a531240b15784b2e
graehl <graehl@7f356e55-ce0a-0410-be72-bc6dfe96aa1c>
2011-12-22 03:28:39 +0000

    throw
    git-svn-id: https://nlg0.isi.edu/svn/sbmt/trunk/graehl@3580 7f356e55-ce0a-0410-be72-bc6dfe96aa1c

--- shared/verbose_exception.hpp
+++ shared/verbose_exception.hpp
@@ -44,6 +44,12 @@ struct verbose_exception : public std::exception
     mbuf << function << "() [" << file << ":" << line << "]: " << m1 << ' ' << m2 << ' ' << m3 << ' ' << m4 << ".";
     message=mbuf.str();
   }
+  template <class M1,class M2,class M3,class M4,class M5>
+  verbose_exception(const char *fun,const char *fil,unsigned lin,const M1 &m1,const M2 &m2,const M3 &m3,const M4 &m4,const M5 &m5) : file(fil),function(fun),line(lin) {
+    std::stringstream mbuf;
+    mbuf << function << "() [" << file << ":" << line << "]: " << m1 << ' ' << m2 << ' ' << m3 << ' ' << m4 << ' ' << m5 << ".";
+    message=mbuf.str();
+  }
   ~verbose_exception() throw()
   {
   }
