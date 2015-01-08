4260a08b316672b7d6c16aab02213bf40bea09fa
graehl <graehl@gmail.com>
2015-01-07 18:47:21 -0800

    rm

--- shared/set_defaults.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-
-my $member='(?:\w+\.)';
-while(<>) { 
-#    print "$1=$2;\n" if (\&$member(\w+)\)\-\>default_value\(([^,]*)(\s*,"[^"]*")?\),\s*("|$)/);
-    print "$1=false;\n" if (/bool_switch\(\&$member(\w+)\)/);
-}
