0e40508a543924976f589655a0396e7e2d2b4df9
graehl <graehl@gmail.com>
2015-06-17 15:17:03 -0700

    fma lerp

---
+++ shared/lerp.hpp
@@ -0,0 +1,31 @@
+/** \file
+
+    fused multiply-and-add optimized linear interpolation
+
+    https://en.wikipedia.org/wiki/FMA_instruction_set
+
+    we want FMA3 (3 arg) not FMA4 (4 arg)
+    gcc -mfma -mno-fma4
+*/
+
+#ifndef LERP_JG_2015_06_17_HPP
+#define LERP_JG_2015_06_17_HPP
+#pragma once
+
+namespace graehl {
+
+template <class T>
+T fma(T a, T b, T c) {
+  return a * b + c;
+}
+
+/// \return ta*a + (1-ta)*b, optimized for fma
+template <class T>
+T lerp(T a, T b, T ta) {
+  return fma(t, v1, fma(-t, v0, v0));
+}
+
+
+}
+
+#endif
